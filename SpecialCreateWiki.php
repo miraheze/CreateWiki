252781a56c59747e3c6d480069664f543f20399e
Southparkfan <southparkfan223@hotmail.com>
2015-09-12 16:22:45 +0200

    Convert Special:CreateWiki to HTMLForm

--- SpecialCreateWiki.php
+++ SpecialCreateWiki.php
@@ -1,77 +1,89 @@
 <?php
-class SpecialCreateWiki extends SpecialPage {
+class SpecialCreateWiki extends FormSpecialPage {
         function __construct() {
                 parent::__construct( 'CreateWiki', 'createwiki' );
         }
 
-        function execute( $par ) {
-                $request = $this->getRequest();
-                $out = $this->getOutput();
-                $this->setHeaders();
-		$this->checkPermissions();
+	protected function getFormFields() {
+		$par = $this->par;
+		$request = $this->getRequest();
 
-		$this->showInputForm();
+		$formDescriptor = array();
 
-		if ( $request->wasPosted() ) {
-			$this->handleInput();
+		$formDescriptor['dbname'] = array(
+			'label-message' => 'createwiki-label-dbname',
+			'type' => 'text',
+			'default' => $request->getVal( 'cwDBname' ) ? $request->getVal( 'cwDBname' ) : $par,
+			'size' => 20,
+			'required' => true,
+			'validation-callback' => array( __CLASS__, 'validateDBname' ),
+			'name' => 'cwDBname',
+
+		);
+
+		$formDescriptor['founder'] = array(
+			'label-message' => 'createwiki-label-founder',
+			'type' => 'text',
+			'default' => $request->getVal( 'cwFounder' ),
+			'size' => 20,
+			'required' => true,
+			'validation-callback' => array( __CLASS__, 'validateFounder' ),
+			'name' => 'cwFounder',
+		);
+
+		$formDescriptor['sitename'] = array(
+			'label-message' => 'createwiki-label-sitename',
+			'type' => 'text',
+			'default' => $request->getVal( 'cwSitename' ),
+			'size' => 20,
+			'name' => 'cwSitename',
+		);
+
+		// Building a language selector (attribution:
+		// includes/specials/SpecialPageLanguage.php L68)
+		$languages = Language::fetchLanguageNames( null, 'mwfile' );
+		ksort( $languages );
+		$options = array();
+		foreach ( $languages as $code => $name ) {
+			$options["$code - $name"] = $code;
 		}
+		
+		$formDescriptor['language'] = array(
+			'type' => 'select',
+			'options' => $options,
+			'label-message' => 'createwiki-label-language',
+			'default' => $request->getVal( 'cwLanguage' ) ? $request->getVal( 'cwLanguage' ) : 'en',
+			'name' => 'cwLanguage',
+		);
+
+		$formDescriptor['private'] = array(
+			'type' => 'check',
+			'label-message' => 'createwiki-label-private',
+			'name' => 'cwPrivate',
+		);
+
+		$formDescriptor['reason'] = array(
+			'label-message' => 'createwiki-label-reason',
+			'type' => 'text',
+			'default' => $request->getVal( 'wpreason' ),
+			'size' => 45,
+			'required' => true,
+		);
+
+		return $formDescriptor;
 	}
 
-	function showInputForm() {
-		$localpage = $this->getPageTitle()->getLocalUrl();
-		$request = $this->getRequest();
-		$language = $request->getVal( 'cwLanguage' ) ? $request->getVal( 'cwLanguage' ) : 'en';
-		$privateboxchecked = $request->getVal( 'cwPrivate' );
-
-                $form = Xml::openElement( 'form', array( 'action' => $localpage, 'method' => 'post' ) );
-                $form .= '<fieldset><legend>' . $this->msg( 'createwiki' )->escaped() . '</legend>';
-                $form .= Xml::openElement( 'table' );
-                $form .= '<tr><td>' . $this->msg( 'createwiki-label-dbname' )->escaped() . '</td>';
-                $form .= '<td>' . Xml::input( 'cwDBname', 20, $request->getVal( 'cwDBname' ), array( 'required' => '' ) ) . '</td></tr>';
-		$form .= '<tr><td>' . $this->msg( 'createwiki-label-founder' )->escaped() . '</td>';
-		$form .= '<td>' . Xml::input( 'cwFounder', 20, $request->getVal( 'cwFounder' ), array( 'required' => '' ) ) . '</td></tr>';
-                $form .= '<tr><td>' . $this->msg( 'createwiki-label-sitename' )->escaped() . '</td>';
-                $form .= '<td>' . Xml::input( 'cwSitename', 20, $request->getVal( 'cwSitename' ), array( 'required' => '' ) ) . '</td></tr>';
-                $form .= '<tr><td>' . $this->msg( 'createwiki-label-language' )->escaped() . '</td>';
-                $form .= '<td>' . Xml::languageSelector( $language, true, null, array( 'name' => 'cwLanguage' ) )[1]  . '</td></tr>';
-                $form .= '<tr><td>' . $this->msg( 'createwiki-label-private' )->escaped() . '</td>';
-                $form .= '<td>' . Xml::check( 'cwPrivate', $privateboxchecked, array( 'value' => 0 ) ) . '</td></tr>';
-                $form .= '<tr><td>' . $this->msg( 'createwiki-label-reason' )->escaped() . '</td>';
-                $form .= '<td>' . Xml::input( 'cwReason', 45, $request->getVal( 'cwReason' ), array( 'required' => '' ) ) . '</td></tr>';
-                $form .= '<tr><td>' . Xml::submitButton( $this->msg( 'createwiki-label-submit' )->plain() ) . '</td></tr>';
-                $form .= Xml::closeElement( 'table' );
-                $form .= '</fieldset>';
-                $form .= Html::hidden( 'cwToken', $this->getUser()->getEditToken() );
-                $form .= Xml::closeElement( 'form' );
-
-                $this->getOutput()->addHTML( $form );
-	}
-
-	function handleInput() {
+	public function onSubmit( array $formData ) {
 		global $IP, $wgCreateWikiSQLfiles;
-
-		$request = $this->getRequest();
-		$out = $this->getOutput();
-
-		$DBname = trim( $request->getVal( 'cwDBname' ) );
-		$founder = trim( $request->getVal( 'cwFounder' ) );
-		$sitename = trim( $request->getVal( 'cwSitename' ) );
-		$reason = $request->getVal( 'cwReason' );
-		$language = $request->getVal( 'cwLanguage' );
-		$private = is_null( $request->getVal( 'cwPrivate' ) ) ? 0 : 1;
+		
+		$DBname = $formData['dbname'];
+		$founderName = $formData['founder'];
+		$siteName = $formData['sitename'];
+		$language = $formData['language'];
+		$private = $formData['private'];
+		$reason = $formData['reason'];
 
 		$dbw = wfGetDB( DB_MASTER );
-
-		if ( !$this->getUser()->matchEditToken( $request->getVal( 'cwToken' ) ) ) {
-                        $out->addWikiMsg( 'createwiki-error-csrf' );
-                        return false;
-                }
-
-		$validation = $this->validateInput( $DBname, $founder );
-
-		if ( !$validation ) {
-			return false;
-		}
 
 		$farmerLogEntry = new ManualLogEntry( 'farmer', 'createwiki' );
 		$farmerLogEntry->setPerformer( $this->getUser() );
@@ -93,43 +105,37 @@ class SpecialCreateWiki extends SpecialPage {
 			$dbw->sourceFile( $sqlfile );
 		}
 
-		$this->writeToDBlist( $DBname, $sitename, $language, $private );
+		$this->writeToDBlist( $DBname, $siteName, $language, $private );
 		$this->createMainPage( $language );
 
-		$shcreateaccount = exec( "/usr/bin/php $IP/extensions/CentralAuth/maintenance/createLocalAccount.php " . wfEscapeShellArg( $founder ) . ' --wiki ' . wfEscapeShellArg( $DBname ) );
+		$shcreateaccount = exec( "/usr/bin/php " .
+			"$IP/extensions/CentralAuth/maintenance/createLocalAccount.php " . wfEscapeShellArg( $founderName ) . " --wiki " . wfEscapeShellArg( $DBname ) );
 
 		if ( !strpos( $shcreateaccount, 'created' ) ) {
 			wfDebugLog( 'CreateWiki', 'Failed to create local account for founder. - error: ' . $shcreateaccount );
-
-			$out->addHTML( '<div class="errorbox">' . $this->msg( 'createwiki-error-usernotcreated' )->escaped() . '</div>' );
-			return false;
+			return wfMessage( 'createwiki-error-usernotcreated' )->escaped();
 		}
 
-		$shpromoteaccount = exec( "/usr/bin/php $IP/maintenance/createAndPromote.php " . wfEscapeShellArg( $founder ) . ' --bureaucrat --sysop --force --wiki ' . wfEscapeShellArg( $DBname ) );
+		$shpromoteaccount = exec( "/usr/bin/php " .
+			"$IP/maintenance/createAndPromote.php " . wfEscapeShellArg( $founderName ) . " --bureaucrat --sysop --force --wiki " . wfEscapeShellArg( $DBname ) );
 
-		$out->addHTML( '<div class="successbox">' . $this->msg( 'createwiki-success' )->escaped() . '</div>' );
+		$this->getOutput()->addHTML( '<div class="successbox">' . wfMessage( 'createwiki-success' )->escaped() . '</div>' );
+		
 		return true;
 	}
 
-	function validateInput( $DBname, $founder ) {
-		$out = $this->getOutput();
-
-		$user = User::newFromName( $founder );
-		if ( !$user->getId() ) {
-			$out->addHTML( '<div class="errorbox">' . $this->msg( 'createwiki-error-foundernonexistent' )->escaped() . '</div>' );
-			return false;
-		}
-
-		if ( !$this->validateDBname( $DBname ) ) {
-			return false;
-		}
-
-		return true;
-	}
-
-	function validateDBname( $DBname ) {
+	public function validateDBname( $DBname, $allData ) {
 		global $wgConf;
-		$out = $this->getOutput();
+
+		# HTMLForm's validation-callback somehow gets called, even
+		# while the form was not submitted yet. This should prevent
+		# the validation from failing because the submitted value is
+		# NULL, but it is a hack, and instead the validation just
+		# shouldn't be called unless the form actually has been
+		# submitted..
+		if ( is_null( $DBname ) ) {
+			return true;
+		}
 
 		$suffixed = false;
 		foreach ( $wgConf->suffixes as $suffix ) {
@@ -143,56 +149,74 @@ class SpecialCreateWiki extends SpecialPage {
 		$res = $dbw->query( 'SHOW DATABASES LIKE ' . $dbw->addQuotes( $DBname ) . ';' );
 
 		if ( $res->numRows() !== 0 ) {
-                        $out->addHTML( '<div class="errorbox">' . $this->msg( 'createwiki-error-dbexists' )->escaped() . '</div>' );
-                        return false;
+			return wfMessage( 'createwiki-error-dbexists' )->escaped();
 		}
 
 		if ( !$suffixed ) {
-			$out->addHTML( '<div class="errorbox">' . $this->msg( 'createwiki-error-notsuffixed' )->escaped() . '</div>' );
-			return false;
+			return wfMessage( 'createwiki-error-notsuffixed' )->escaped();
 		}
 
 		if ( !ctype_alnum( $DBname ) ) {
-			$out->addHTML( '<div class="errorbox">' . $this->msg( 'createwiki-error-notalnum' )->escaped() . '</div>' );
-			return false;
+			return wfMessage( 'createwiki-error-notalnum' )->escaped();
 		}
 
 		if ( strtolower( $DBname ) !== $DBname ) {
-			$out->addHTML( '<div class="errorbox">' . $this->msg( 'createwiki-error-notlowercase' )->escaped() . '</div>' );
-			return false;
+			return wfMessage( 'createwiki-error-notlowercase' )->escaped();
 		}
 
 		return true;
 	}
 
-	function writeToDBlist( $DBname, $sitename, $language, $private ) {
+	public function validateFounder( $founderName, $allData ) {
+		# HTMLForm's validation-callback somehow gets called, even
+                # while the form was not submitted yet. This should prevent
+                # the validation from failing because the submitted value is
+                # NULL, but it is a hack, and instead the validation just
+                # shouldn't be called unless the form actually has been
+                # submitted..
+		if ( is_null( $founderName ) ) {
+			return true;
+		}
+		
+		$user = User::newFromName( $founderName );
+
+		if ( !$user->getId() ) {
+			return wfMessage( 'createwiki-error-foundernonexistent' )->escaped();
+		}
+
+		return true;
+	}
+
+	public function writeToDBlist( $DBname, $siteName, $language, $private ) {
 		global $IP;
 
-		$dbline = "$DBname|$sitename|$language|\n";
+		$dbline = "$DBname|$siteName|$language|\n";
 		file_put_contents( "/srv/mediawiki/dblist/all.dblist", $dbline, FILE_APPEND | LOCK_EX );
 
-		if ( $private !== 0 ) {
+		if ( $private ) {
 			file_put_contents( "/srv/mediawiki/dblist/private.dblist", "$DBname\n", FILE_APPEND | LOCK_EX );
 		}
 
 		return true;
 	}
 
-	function createMainPage( $lang ) {
+	public function createMainPage( $language ) {
 		// Don't use Meta's mainpage message!
-		if ( $lang !== 'en' ) {
+		if ( $language !== 'en' ) {
 			$page = wfMessage( 'mainpage' )->inLanguage( $lang )->plain();
 		} else {
 			$page = 'Main_Page';
 		}
-		
+
 		$title = Title::newFromText( $page );
 		$article = WikiPage::factory( $title );
 
 		$article->doEditContent( new WikitextContent(
-				wfMessage( 'createwiki-defaultmainpage' )->inLanguage( $lang )->plain() ),
-			'Create main page',
+			wfMessage( 'createwiki-defaultmainpage' )->inLanguage( $language )->plain() ), // Text
+			'Create main page', // Edit summary
 			EDIT_NEW
 		);
+
+		return true;
 	}
 }
