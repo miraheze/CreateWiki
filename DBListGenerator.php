a30d322605ecf29b5937afbd63a3bdc9767407df
Southparkfan <Southparkfan@users.noreply.github.com>
2015-11-09 18:55:06 +0100

    DBListGenerator: do not truncate before looping over wikis array

--- DBListGenerator.php
+++ DBListGenerator.php
@@ -2,45 +2,46 @@
 require_once '/srv/mediawiki/w/maintenance/commandLine.inc';
 
 if ( $wgDBname !== 'metawiki' ) {
-        throw new MWException( 'The DBname used for this script must be metawiki.' );
+	throw new MWException( 'The DBname used for this script must be metawiki.' );
 }
 
 $dbw = wfGetDB( DB_MASTER );
 
 $res = $dbw->select(
-                'cw_wikis',
-                '*',
-                array(),
-                __METHOD__
+	'cw_wikis',
+	'*',
+	array(),
+	__METHOD__
 );
 
 if ( !$res || !is_object( $res ) ) {
-        throw new MWException( '$res was not set to a valid array.' );
+	throw new MWException( '$res was not set to a valid array.' );
 }
 
 $DBlistPath = '/srv/mediawiki/dblist';
 
-// Truncate the lists first
-foreach ( array( 'all.dblist', 'private.dblist', 'closed.dblist' ) as $DBlist ) {
-        file_put_contents( "$DBlistPath/$DBlist", '' );
-}
+$allWikis = array();
+$privateWikis = array();
+$closedWikis = array();
 
 foreach ( $res as $row ) {
-        $DBname = $row->wiki_dbname;
-        $siteName = $row->wiki_sitename;
-        $language = $row->wiki_language;
-        $private = $row->wiki_private;
-        $closed = $row->wiki_closed;
-
-        $allDBlistString = "$DBname|$siteName|$language|\n";
-
-        file_put_contents( "$DBlistPath/all.dblist", $allDBlistString, FILE_APPEND | LOCK_EX );
-
-        if ( $private === "1" ) {
-                file_put_contents( "$DBlistPath/private.dblist", $DBname . PHP_EOL, FILE_APPEND | LOCK_EX );
-        }
-
-        if ( $closed === "1" ) {
-                file_put_contents( "$DBlistPath/closed.dblist", $DBname . PHP_EOL, FILE_APPEND | LOCK_EX );
-        }
+	$DBname = $row->wiki_dbname;
+	$siteName = $row->wiki_sitename;
+	$language = $row->wiki_language;
+	$private = $row->wiki_private;
+	$closed = $row->wiki_closed;
+	
+	$allWikis[] = "$DBname|$siteName|$language|";
+	
+	if ( $private === "1" ) {
+		$privateWikis[] = $DBname;
+	}
+	
+	if ( $closed === "1" ) {
+		$closedWikis[] = $DBname;
+	}
+	
+	file_put_contents( "$DBlistPath/all.dblist", implode( "\n", $allWikis ), LOCK_EX );
+	file_put_contents( "$DBlistPath/private.dblist", implode( "\n", $privateWikis ), LOCK_EX );
+	file_put_contents( "$DBlistPath/closed.dblist", implode( "\n", $closedWikis ), LOCK_EX );
 }
