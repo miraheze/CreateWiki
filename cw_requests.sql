19d5823120a13b06cbb4c39c8545b5f76195c71d
Brent Laabs <blaabs@perceptyx.com>
2016-05-01 03:09:59 -0700

    Bump sitename to varchar(128) and dbname to VARCHAR(64) consistently,
    Make wiki_dbname a primary key on cw_wikis

--- cw_requests.sql
+++ cw_requests.sql
@@ -1,11 +1,11 @@
 CREATE TABLE /*_*/cw_requests (
   cw_id INT NULL AUTO_INCREMENT PRIMARY KEY NOT NULL,
   cw_comment TEXT DEFAULT NULL,
-  cw_dbname VARCHAR(32) DEFAULT NULL,
+  cw_dbname VARCHAR(64) DEFAULT NULL,
   cw_ip VARCHAR(64) NOT NULL,
   cw_language VARCHAR(12) NOT NULL,
   cw_private SMALLINT DEFAULT NULL,
-  cw_sitename VARCHAR(32) NOT NULL,
+  cw_sitename VARCHAR(128) NOT NULL,
   cw_status VARCHAR(16) DEFAULT NULL,
   cw_status_comment VARCHAR(512) DEFAULT NULL,
   cw_status_comment_timestamp VARCHAR(32) DEFAULT NULL,
--- cw_wikis.sql
+++ cw_wikis.sql
@@ -1,6 +1,6 @@
 CREATE TABLE /*_*/cw_wikis (
-  `wiki_dbname` VARCHAR(64) NOT NULL,
-  `wiki_sitename` VARCHAR(32) NOT NULL,
+  `wiki_dbname` VARCHAR(64) NOT NULL PRIMARY KEY,
+  `wiki_sitename` VARCHAR(128) NOT NULL,
   `wiki_language` VARCHAR(12) NOT NULL,
   `wiki_private` SMALLINT NOT NULL,
   `wiki_closed` SMALLINT NOT NULL
