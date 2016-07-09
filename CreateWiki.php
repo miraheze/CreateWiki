446944d6906f9162905cc433b95e31cbdd8c417c
John F. Lewis <johnflewis93@gmail.com>
2016-07-09 22:36:01 +0100

    convert CreateWiki to extension registration + clean up SQLfiles param

--- CreateWiki.php
+++ CreateWiki.php
@@ -1,41 +1,7 @@
 <?php
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( 'Not an entry point.' );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'CreateWiki' );
+	return;
+} else {
+	die( 'This version requires MediaWiki 1.25+' );
 }
-
-$wgExtensionCredits['specialpage'][] = array(
-	'author' => 'Southparkfan',
-	'descriptionmsg' => 'createwiki-desc',
-	'name' => 'CreateWiki',
-	'path' => __FILE__,
-	'url' => '//github.com/Miraheze/CreateWiki'
-);
-
-$wgAutoloadClasses['CreateWikiHooks'] = __DIR__ . '/CreateWiki.hooks.php';
-$wgAutoloadClasses['CreateWikiLogFormatter'] = __DIR__ . '/CreateWikiLogFormatter.php';
-$wgAutoloadClasses['RequestWikiQueuePager'] = __DIR__ . '/RequestWikiQueuePager.php';
-$wgAutoloadClasses['SpecialCreateWiki'] = __DIR__ . '/SpecialCreateWiki.php';
-$wgAutoloadClasses['SpecialManageWiki'] = __DIR__ . '/SpecialManageWiki.php';
-$wgAutoloadClasses['SpecialRequestWiki'] = __DIR__ . '/SpecialRequestWiki.php';
-$wgAutoloadClasses['SpecialRequestWikiQueue'] = __DIR__ . '/SpecialRequestWikiQueue.php';
-$wgMessagesDirs['CreateWiki'] = __DIR__ . '/i18n';
-$wgSpecialPages['CreateWiki'] = 'SpecialCreateWiki';
-$wgSpecialPages['ManageWiki'] = 'SpecialManageWiki';
-$wgSpecialPages['RequestWiki'] = 'SpecialRequestWiki';
-$wgSpecialPages['RequestWikiQueue'] = 'SpecialRequestWikiQueue';
-
-$wgHooks['LoadExtensionSchemaUpdates'][] = 'CreateWikiHooks::fnCreateWikiSchemaUpdates';
-
-$wgAvailableRights[] = array( 'createwiki', 'managewiki' );
-$wgLogTypes[] = 'farmer';
-$wgLogActionsHandlers['farmer/createwiki'] = 'LogFormatter';
-$wgLogActionsHandlers['farmer/requestwiki'] = 'CreateWikiLogFormatter';
-$wgLogActionsHandlers['farmer/managewiki'] = 'LogFormatter';
-
-$wgCreateWikiSQLfiles = array(
-	"$IP/maintenance/tables.sql",
-	"$IP/extensions/AbuseFilter/abusefilter.tables.sql",
-	"$IP/extensions/AntiSpoof/sql/patch-antispoof.mysql.sql",
-	"$IP/extensions/CheckUser/cu_log.sql",
-	"$IP/extensions/CheckUser/cu_changes.sql"
-);
