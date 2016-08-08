c1e65f26a0029c2ba8e500c66e1cb4db694cde53
Reception123 <motoc.luca@gmail.com>
2016-08-08 21:03:56 +0300

    Revert "Declare new permissions so that they are available to CentralAuth"
    Not the issue. Discussed on IRC

--- CreateWiki.php
+++ CreateWiki.php
@@ -5,9 +5,3 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 } else {
 	die( 'This version requires MediaWiki 1.25+' );
 }
-
-// We must declare available rights, otherwise they are not available to other extensions, such as CentralAuth
-
-$wgAvailablePermissions[] = 'createwiki';
-$wgAvailablePermissions[] = 'managewiki';
-$wgAvailablePermissions[] = 'managewiki-restricted';
