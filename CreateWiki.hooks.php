64141480d6978412b293cebbaaa7aefc6f9e71b8
LegoFan4000 <paulfkeffer@gmail.com>
2016-11-28 20:45:34 -0500

    Fix createwiki for MW 1.28

--- CreateWiki.hooks.php
+++ CreateWiki.hooks.php
@@ -1,6 +1,6 @@
 <?php
 class CreateWikiHooks {
-	function fnCreateWikiSchemaUpdates( DatabaseUpdater $updater ) {
+	public static function fnCreateWikiSchemaUpdates( DatabaseUpdater $updater ) {
 		$updater->addExtensionTable( 'cw_requests',
 			__DIR__ . '/cw_requests.sql' );
 		$updater->addExtensionTable( 'cw_wikis',
