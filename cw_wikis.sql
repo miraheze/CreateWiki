749f1d1bef0b6a1e75a6af4eb5d8b7a2f8addcd9
Corey Chambers <coreychambers123@gmail.com>
2017-06-19 17:50:19 -0400

    wiki_favicon

--- cw_wikis.sql
+++ cw_wikis.sql
@@ -1,5 +1,6 @@
 CREATE TABLE /*_*/cw_wikis (
   `wiki_dbname` VARCHAR(64) NOT NULL PRIMARY KEY,
+  `wiki_favicon` VARCHAR(255) NOT NULL,
   `wiki_sitename` VARCHAR(128) NOT NULL,
   `wiki_language` VARCHAR(12) NOT NULL,
   `wiki_logo` VARCHAR(255) NOT NULL DEFAULT 'https://static.miraheze.org/metawiki/3/35/Miraheze_Logo.svg',
