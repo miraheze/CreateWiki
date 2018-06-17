8953e0240c6000d03543e97d0ba39b19180448b9
paladox <paladox@users.noreply.github.com>
2018-06-17 23:49:31 +0100

    Fix path to sql files

--- includes/CreateWikiHooks.php
+++ includes/CreateWikiHooks.php
@@ -2,11 +2,11 @@
 class CreateWikiHooks {
 	public static function fnCreateWikiSchemaUpdates( DatabaseUpdater $updater ) {
 		$updater->addExtensionTable( 'cw_requests',
-			__DIR__ . '/cw_requests.sql' );
+			__DIR__ . '/../sql/cw_requests.sql' );
 		$updater->addExtensionTable( 'cw_wikis',
-			__DIR__ . '/cw_wikis.sql' );
+			__DIR__ . '/../sql/cw_wikis.sql' );
 		$updater->addExtensionTable( 'cw_comments',
-			__DIR__ . '/cw_comments.sql' );
+			__DIR__ . '/../sql/cw_comments.sql' );
 
 		return true;
 	}
