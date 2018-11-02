cfd01fcca9c5b5946f0ccfd0f8872016e6abab97
paladox <paladox@users.noreply.github.com>
2018-11-02 01:44:09 +0000

    Update SpecialCreateWiki.php

--- includes/CreateWiki/SpecialCreateWiki.php
+++ includes/CreateWiki/SpecialCreateWiki.php
@@ -244,7 +244,7 @@ class SpecialCreateWiki extends FormSpecialPage {
 				'wiki_sitename' => $siteName,
 				'wiki_language' => $language,
 				'wiki_private' => $private,
-				'wiki_creation' => wfGetDB()->timestamp(),
+				'wiki_creation' => $dbw->timestamp(),
 				'wiki_closed' => 0,
 				'wiki_inactive' => 0,
 				'wiki_category' => $category,
