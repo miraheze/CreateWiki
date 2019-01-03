578e6bc1219fc94b1dbe35a73c007790e51637d2
The-Voidwalker <voidwalker.wikipedia@gmail.com>
2019-01-03 18:26:25 -0500

    increase size of userids (#101)
    * up size of user ids
    
    * up size of user ids

--- sql/cw_requests.sql
+++ sql/cw_requests.sql
@@ -9,7 +9,7 @@ CREATE TABLE /*_*/cw_requests (
   cw_status VARCHAR(16) DEFAULT NULL,
   cw_timestamp varchar(32) NOT NULL,
   cw_url VARCHAR(96) NOT NULL,
-  cw_user SMALLINT NOT NULL,
+  cw_user INT(10) NOT NULL,
   cw_custom VARCHAR(96) NOT NULL,
   cw_category VARCHAR(64) NOT NULL,
   cw_visibility TINYINT UNSIGNED NOT NULL DEFAULT '0'
