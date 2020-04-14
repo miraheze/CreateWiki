4418ecba9d0dc1fe122ee17b98cc3d80b131d1b9
paladox <paladox@users.noreply.github.com>
2020-04-14 22:26:31 +0100

    Update WikiInitialise.php

--- includes/WikiInitialise.php
+++ includes/WikiInitialise.php
@@ -69,7 +69,9 @@ class WikiInitialise {
 
 		$suffixMatch = array_flip( $siteMatch );
 		$domainsMatch = array_flip( $databasesArray['domains'] );
-		$serverArray = [];
+		$serverArray = [
+			'default' => $suffixMatch['wiki'],
+		];
 
 		// MediaWiki has a cross-wiki depedency in wikifarms. So we need to know what else exists here, but not their real domains - just accessible ones
 		foreach ( $databasesArray['databases'] as $db ) {
@@ -81,7 +83,6 @@ class WikiInitialise {
 		}
 
 		$this->config->settings['wgServer'] = array_merge( $serverArray, $domainsMatch );
-		$this->config->settings['wgServer']['default'] = 'https://example.org';
 
 		// We need the CLI to be able to access 'deleted' wikis
 		if ( PHP_SAPI == 'cli' && file_exists( $this->cacheDir . '/deleted.json' ) ) {
