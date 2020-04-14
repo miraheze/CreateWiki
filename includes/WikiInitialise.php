8f9f7fb0e2b9f930d4bde3123e643067cae1a8bf
paladox <paladox@users.noreply.github.com>
2020-04-14 22:45:39 +0100

    Set a default for wgServer - fallback incase wgServer is not set (#135)

--- includes/WikiInitialise.php
+++ includes/WikiInitialise.php
@@ -69,7 +69,9 @@ class WikiInitialise {
 
 		$suffixMatch = array_flip( $siteMatch );
 		$domainsMatch = array_flip( $databasesArray['domains'] );
-		$serverArray = [];
+		$serverArray = [
+			'default' => 'https://' . $suffixMatch[ array_key_first( $suffixMatch ) ],
+		];
 
 		// MediaWiki has a cross-wiki depedency in wikifarms. So we need to know what else exists here, but not their real domains - just accessible ones
 		foreach ( $databasesArray['databases'] as $db ) {
