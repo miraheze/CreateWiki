36308dc1d2fa9b087d1806122364bfad2d2a756f
paladox <paladox@users.noreply.github.com>
2020-07-10 14:56:04 +0100

    Concert cw_comments.cw_comment to a TEXT column
    This ups the maximum characters from 500 to 65,535.
    
    Bug: T5898

--- sql/cw_comments.sql
+++ sql/cw_comments.sql
@@ -1,6 +1,6 @@
 CREATE TABLE /*_*/cw_comments (
   cw_id INT NOT NULL,
-  cw_comment VARCHAR(512) NOT NULL,
+  cw_comment TEXT NOT NULL,
   cw_comment_timestamp VARCHAR(32) NOT NULL,
   cw_comment_user INT(10) NOT NULL
 ) /*$wgDBTableOptions*/;
