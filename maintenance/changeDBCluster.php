343cb2ba21494b3fa62d135319f34ed3867d4772
John Lewis <johnflewis93@gmail.com>
2020-10-15 18:41:27 +0100

    Convert to new RemoteWiki code

--- maintenance/changeDBCluster.php
+++ maintenance/changeDBCluster.php
@@ -8,18 +8,18 @@ require_once "$IP/maintenance/Maintenance.php";
 
 use MediaWiki\MediaWikiServices;
 
-class ChangeDBCluster extends Maintenance {
-	private $dbw = null;
-
-	public function __construct() {
+class ChangeDBCluster extends Maintenance
+{
+	public function __construct()
+	{
 		parent::__construct();
 		$this->addOption( 'db-cluster', 'Sets the wikis requested to a different db cluster.', true, true );
 		$this->addOption( 'file', 'Path to file where the wikinames are store. Must be one wikidb name per line. (Optional, fallsback to current dbname)', false, true );
 	}
 
-	public function execute() {
+	public function execute()
+	{
 		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'createwiki' );
-		$this->dbw = wfGetDB( DB_MASTER, [], $config->get( 'CreateWikiDatabase' ) );
 
 		if ( (bool)$this->getOption( 'file' ) ) {
 			$file = fopen( $this->getOption( 'file' ), 'r' );
@@ -27,10 +27,9 @@ class ChangeDBCluster extends Maintenance {
 				$this->fatalError( "Unable to read file, exiting" );
 			}
 		} else {
-			$this->updateDbCluster( $config->get( 'DBname' ) );
-
-			$this->recacheDBListJson( $config->get( 'DBname' ) );
-			$this->recacheWikiJson( $config->get( 'DBname' ) );
+			$wiki = new RemoteWiki( $config->get( 'DBname' ) );
+			$wiki->setDBCluster( $this->getOption( 'db-cluster' ) );
+			$wiki->commit();
 			return;
 		}
 
@@ -39,37 +38,10 @@ class ChangeDBCluster extends Maintenance {
 			if ( $line == '' ) {
 				continue;
 			}
-
-			$this->updateDbCluster( $line );
-			$this->recacheWikiJson( $line );
+			$wiki = new RemoteWiki( $line );
+			$wiki->setDBCluster( $this->getOption( 'db-cluster' ) );
+			$wiki->commit();
 		}
-
-		$this->recacheDBListJson( $config->get( 'DBname' ) );
-	}
-
-	private function updateDbCluster( string $wiki ) {
-		$this->dbw->update(
-			'cw_wikis',
-			[
-				'wiki_dbcluster' => (string)$this->getOption( 'db-cluster' ),
-			],
-			[
-				'wiki_dbname' => $wiki,
-			],
-			__METHOD__
-		);
-	}
-
-	private function recacheWikiJson( string $wiki ) {
-		$cWJ = new CreateWikiJson( $wiki );
-		$cWJ->resetWiki();
-		$cWJ->update();
-	}
-
-	private function recacheDBListJson( string $wiki ) {
-		$cWJ = new CreateWikiJson( $wiki );
-		$cWJ->resetDatabaseList();
-		$cWJ->update();
 	}
 }
 
