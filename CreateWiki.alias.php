59d7378c05b781978516c5d03bf3862fbb764fe0
Reception123 <utilizator.receptie123@gmail.com>
2020-11-18 16:56:45 +0100

    add alias page to CreateWiki and redirect RequestWikiEdit to RequestWikiQueue
    per T6436

---
+++ CreateWiki.alias.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Aliases for special pages of CreateWiki extension.
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = [
+	'CreateWiki' => [ 'CreateWiki' ],
+	'RequestWiki' => [ 'RequestWiki' ],
+	'RequestWikiQueue' => [ 'RequestWikiQueue', 'RequestWikiEdit' ],
+];
