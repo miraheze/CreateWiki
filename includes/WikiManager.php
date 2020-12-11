d29bee0aea69baa6428d0d95f31f1942f9253966
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2020-12-11 12:53:28 -0700

    $reason -> $description

--- includes/WikiManager.php
+++ includes/WikiManager.php
@@ -76,7 +76,6 @@ class WikiManager {
 		string $category,
 		string $requester,
 		string $actor,
-		string $reason
 	) {
 		$wiki = $this->dbname;
 
@@ -156,7 +155,7 @@ class WikiManager {
 
 		$this->notificationsTrigger( 'creation', $wiki, [ 'siteName' => $siteName ], $requester );
 
-		$this->logEntry( 'farmer', 'createwiki', $actor, $reason, [ '4::wiki' => $wiki ] );
+		$this->logEntry( 'farmer', 'createwiki', $actor, $description, [ '4::wiki' => $wiki ] );
 
 		return null;
 	}
@@ -265,13 +264,13 @@ class WikiManager {
 		return ( $error ) ? wfMessage( 'createwiki-error-' . $error )->escaped() : false;
 	}
 
-	private function logEntry( string $log, string $action, string $actor, string $reason, array $params, string $loggingWiki = null ) {
+	private function logEntry( string $log, string $action, string $actor, string $description, array $params, string $loggingWiki = null ) {
 		$logDBConn = wfGetDB( DB_MASTER, [], $loggingWiki ?? $this->config->get( 'CreateWikiGlobalWiki' ) );
 
 		$logEntry = new ManualLogEntry( $log, $action );
 		$logEntry->setPerformer( User::newFromName( $actor ) );
 		$logEntry->setTarget( Title::newFromID( 1 ) );
-		$logEntry->setComment( $reason );
+		$logEntry->setComment( $description );
 		$logEntry->setParameters( $params );
 		$logID = $logEntry->insert( $logDBConn );
 		$logEntry->publish( $logID );
