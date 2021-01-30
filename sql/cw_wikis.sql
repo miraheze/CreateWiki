046728620d48014beac05ebf92a506a57a8980f6
paladox <paladox@users.noreply.github.com>
2021-01-30 17:09:30 +0000

    Remove 'wiki_settings' and 'wiki_extensions' from cw_wikis
    Both are unused and were moved to mw_settings (ManageWiki)

--- sql/cw_wikis.sql
+++ sql/cw_wikis.sql
@@ -13,10 +13,8 @@ CREATE TABLE /*_*/cw_wikis (
   wiki_deleted SMALLINT NOT NULL,
   wiki_deleted_timestamp BINARY(14) NULL,
   wiki_locked TINYINT NOT NULL,
-  wiki_settings LONGTEXT NULL,
   wiki_dbcluster VARCHAR(5) DEFAULT 'c1',
-  wiki_category VARCHAR(64) NOT NULL,
-  wiki_extensions MEDIUMTEXT NULL
+  wiki_category VARCHAR(64) NOT NULL
 ) /*$wgDBTableOptions*/;
 
 CREATE INDEX /*i*/wiki_dbname ON /*_*/cw_wikis (wiki_dbname);
