29279f5ba7a24610833159ed1b3d4e19d79b9402
John F. Lewis <johnflewis93@gmail.com>
2021-03-13 20:20:55 +0000

    AI Implementation (#200)
    * Check against Threshold Variable + support job only
    
    * add config
    
    * add AI code to AI job with ability to auto-approve
    
    * fix
    
    * fix
    
    * third fix, c&p is bad
    
    * add double if for safeguarding

--- includes/RequestWiki/RequestWikiAIJob.php
+++ includes/RequestWiki/RequestWikiAIJob.php
@@ -1,13 +1,34 @@
 <?php
 
+use MediaWiki\MediaWikiServices;
+use Phpml\ModelManager;
+
 class RequestWikiAIJob extends Job {
 	public function __construct( Title $title, array $params ) {
 		parent::__construct( 'RequestWikiAIJob', $params );
 	}
 
 	public function run() {
+		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'createwiki' );
+		$modelFile = $config->get( 'CreateWikiPersistentModelFile' );
+		
 		$wr = new WikiRequest( $this->params['id'] );
-		$wr->tryAutoCreate( false );
+
+		if ( file_exists( $modelFile ) ) {
+			$modelManager = new ModelManager();
+			$pipeline = $modelManager->restoreFromFile( $modelFile );
+			$tokenDescription = (array)strtolower( $this->description );
+			$pipeline->transform( $tokenDescription );
+			$approveScore = $pipeline->getEstimator()->predictProbability( $tokenDescription )[0]['approved'];
+
+			$wr->addComment( 'Approval Score: ' . (string)round( $approveScore, 2 ), User::newSystemUser( 'CreateWiki Extension' ) );
+			
+			if ( is_int( $config->get( 'CreateWikiAIThreshold' ) ) && ( (int)round( $approveScore, 2 ) > $config->get( 'CreateWikiAIThreshold' ) ) ) {
+				$wr->approve( User::newSystemUser( 'CreateWiki Extension' ) );
+			}
+				
+		}
+		
 		return true;
 	}
 }
