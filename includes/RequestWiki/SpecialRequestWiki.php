d4076db2cac2642d488384201a6a81c955816705
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-03-29 15:08:42 -0700

    Default CreateWikiPurposes to ''

--- includes/RequestWiki/SpecialRequestWiki.php
+++ includes/RequestWiki/SpecialRequestWiki.php
@@ -88,6 +88,7 @@ class SpecialRequestWiki extends FormSpecialPage {
 				'type' => 'select',
 				'label-message' => 'requestwiki-label-purpose',
 				'options' => $this->config->get( 'CreateWikiPurposes')
+				'default' => '',
 			];
 		}
 
