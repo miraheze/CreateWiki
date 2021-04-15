613fc3b4cc4b6ea53204f8dab452bfc0430f94ae
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-04-15 13:16:43 -0600

    Update DBListGenerator.php

--- maintenance/DBListGenerator.php
+++ maintenance/DBListGenerator.php
@@ -12,7 +12,7 @@ class CreateWikiDBListGenerator extends Maintenance {
 
 	public function execute() {
 		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'createwiki' );
-		$dbw = wfGetDB( DB_MASTER, [], $config->get( 'CreateWikiDatabase' ) );
+		$dbw = wfGetDB( DB_PRIMARY, [], $config->get( 'CreateWikiDatabase' ) );
 
 		$res = $dbw->select(
 			'cw_wikis',
