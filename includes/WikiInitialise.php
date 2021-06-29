881eb5b7be6ea3a9d46ccdaaa44d082a1e713087
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-06-28 19:42:07 -0600

    Update WikiInitialise.php

--- includes/WikiInitialise.php
+++ includes/WikiInitialise.php
@@ -172,9 +172,9 @@ class WikiInitialise {
 
 				foreach ( $this->config->get( 'ManageWikiExtensions' ) as $name => $ext ) {
 					if ( $ext['var'] === $var ) {
-						$path = array_column( $credits, 'path', 'name' )[ $ext['name'] ] ?? false;
+						// $path = array_column( $credits, 'path', 'name' )[ $ext['name'] ] ?? false;
 						if ( $path ) {
-							$pathInfo = pathinfo( $path );
+						//	$pathInfo = pathinfo( $path );
 							/*$pathInfo['extension'] === 'php' ? require_once $path : ( $pathInfo['filename'] === 'extension' ?
 								wfLoadExtension( pathinfo( dirname( $path ) ) ) : wfLoadSkin( pathinfo( dirname( $path ) ) )
 							);*/
