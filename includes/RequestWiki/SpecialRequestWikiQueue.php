36774a0fcc460b15222ff32ea0002b290f0beaeb
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-22 18:30:05 -0600

    Update SpecialRequestWikiQueue.php

--- includes/RequestWiki/SpecialRequestWikiQueue.php
+++ includes/RequestWiki/SpecialRequestWikiQueue.php
@@ -13,11 +13,9 @@ class SpecialRequestWikiQueue extends SpecialPage {
 			);
 		}
 
-		if ( RequestContext::getMain()->getTitle() == null ) {
-			RequestContext::getMain()->setTitle(
-				SpecialPage::getTitleFor( 'RequestWikiQueue' )
-			);
-		}
+		RequestContext::getMain()->setTitle(
+			SpecialPage::getTitleFor( 'RequestWikiQueue' )
+		);
 
 		if ( is_null( $par ) || $par === '' ) {
 			$this->doPagerStuff();
