a483bd5b59cacfd3afbe266841610a9a282feeea
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-26 13:03:04 -0600

    Update populateWikiCreation.php

--- maintenance/populateWikiCreation.php
+++ maintenance/populateWikiCreation.php
@@ -1,36 +1,20 @@
 <?php
-/**
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License along
-* with this program; if not, write to the Free Software Foundation, Inc.,
-* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-* http://www.gnu.org/copyleft/gpl.html
-*
-* @file
-* @ingroup Maintenance
-* @author Southparkfan
-* @author John Lewis
-* @author Paladox
-* @version 1.0
-*/
 
-require_once( __DIR__ . '/../../../maintenance/Maintenance.php' );
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+
+require_once "$IP/maintenance/Maintenance.php";
 
 use MediaWiki\MediaWikiServices;
 
 class PopulateWikiCreation extends Maintenance {
 	public function __construct() {
 		parent::__construct();
-		$this->mDescription = "Populates wiki_creation column in cw_wikis table";
+
+		$this->mDescription = 'Populates wiki_creation column in cw_wikis table';
+		$this->requireExtension( 'CreateWiki' );
 	}
 
 	public function execute() {
@@ -58,7 +42,7 @@ class PopulateWikiCreation extends Maintenance {
 				'log_timestamp',
 				[
 					'log_action' => 'createwiki',
-					'log_params' => serialize( ['4::wiki' => $DBname ] )
+					'log_params' => serialize( [ '4::wiki' => $DBname ] )
 				],
 				__METHOD__,
 				[
