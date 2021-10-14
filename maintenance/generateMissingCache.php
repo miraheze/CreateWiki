8a4db79c9a988b3c08c9681df5a038b16bec5d69
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-10-14 16:58:28 -0600

    Create GenerateMissingCache maintenance script (#248)

---
+++ maintenance/generateMissingCache.php
@@ -0,0 +1,35 @@
+<?php
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+
+require_once "$IP/maintenance/Maintenance.php";
+
+use MediaWiki\MediaWikiServices;
+
+class GenerateMissingCache extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+
+		$this->mDescription = 'Generates CreateWiki cache files for all wikis that are currently missing one.';
+		$this->requireExtension( 'CreateWiki' );
+	}
+
+	public function execute() {
+		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'createwiki' );
+
+		foreach ( $config->get( 'LocalDatabases' ) as $db ) {
+			if ( file_exists( $config->get( 'CreateWikiCacheDirectory' ) . '/' . $db . '.json' ) ) {
+				continue;
+			}
+
+			$cWJ = new CreateWikiJson( $db );
+			$cWJ->update();
+		}
+	}
+}
+
+$maintClass = GenerateMissingCache::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
