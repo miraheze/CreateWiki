bf8b533a830b7d990006ba30ab3a00714f6c3ae2
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-10-19 17:11:04 -0600

    Create RebuildExtensionListCache

---
+++ maintenance/rebuildExtensionListCache.php
@@ -0,0 +1,49 @@
+<?php
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+
+require_once "$IP/maintenance/Maintenance.php";
+
+use MediaWiki\MediaWikiServices;
+
+class RebuildExtensionListCache extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+
+		$this->mDescription = 'Rebuild or generate extension-list.json cache file.';
+		$this->requireExtension( 'CreateWiki' );
+	}
+
+	public function execute() {
+		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'createwiki' );
+
+		$queue = array_fill_keys( array_merge(
+				glob( $config->get( 'ExtensionDirectory' ) . '/*/extension*.json' ),
+				glob( $config->get( 'StyleDirectory' ) . '/*/skin.json' )
+			),
+		true );
+
+		$processor = new ExtensionProcessor();
+
+		foreach ( $queue as $path => $mtime ) {
+			$json = file_get_contents( $path );
+			$info = json_decode( $json, true );
+			$version = $info['manifest_version'];
+
+			$processor->extractInfo( $path, $info, $version );
+		}
+
+		$data = $processor->getExtractedInfo();
+
+		$list = array_column( $data['credits'], 'path', 'name' );
+
+		$cacheDir = $config->get( 'CreateWikiCacheDirectory' );
+		file_put_contents( "{$cacheDir}/extension-list.json", json_encode( $list ), LOCK_EX );
+	}
+}
+
+$maintClass = RebuildExtensionListCache::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
