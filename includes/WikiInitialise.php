d77d5cecb8b5194e5082ff98537d164696b6d1fa
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-10-19 17:26:05 -0600

    Create RebuildExtensionListCache maintenance script (#251)

--- includes/WikiInitialise.php
+++ includes/WikiInitialise.php
@@ -221,7 +221,7 @@ class WikiInitialise {
 		}
 	}
 
-	public function loadExtensions( $mtimeFile = false ) {
+	public function loadExtensions() {
 		// If we don't have a cache file, let us exit here
 		if ( !file_exists( $this->cacheDir . '/' . $this->dbname . '.json' ) ) {
 			return;
@@ -231,9 +231,7 @@ class WikiInitialise {
 
 		$config = new GlobalVarConfig( 'wg' );
 
-		if ( !file_exists( "{$this->cacheDir}/extension-list.json" ) ||
-			( $mtimeFile && ( filemtime( "{$this->cacheDir}/extension-list.json" ) <= filemtime( $mtimeFile ) ) )
-		) {
+		if ( !file_exists( "{$this->cacheDir}/extension-list.json" ) ) {
 			$queue = array_fill_keys( array_merge(
 					glob( $config->get( 'ExtensionDirectory' ) . '/*/extension*.json' ),
 					glob( $config->get( 'StyleDirectory' ) . '/*/skin.json' )
