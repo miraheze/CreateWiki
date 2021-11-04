71a51321d12059d1d2b79458e4cfb77f937e272c
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-11-04 16:53:21 -0600

    Remove whitespace

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -7,7 +7,7 @@ return [
 	'CreateWiki.NotificationsManager' => static function ( MediaWikiServices $services ): CreateWikiNotificationsManager {
 		return new CreateWikiNotificationsManager(
 			RequestContext::getMain(),
-			new ServiceOptions( 
+			new ServiceOptions(
 				CreateWikiNotificationsManager::CONSTRUCTOR_OPTIONS,
 				$services->getConfigFactory()->makeConfig( 'createwiki' )
 			),
