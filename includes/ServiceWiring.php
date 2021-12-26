ef708f4dd9f290b42a5cda51a7800c3eee985229
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-12-26 15:29:05 -0700

    Update ServiceWiring.php

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -6,6 +6,7 @@ use MediaWiki\MediaWikiServices;
 return [
 	'CreateWiki.NotificationsManager' => static function ( MediaWikiServices $services ): CreateWikiNotificationsManager {
 		return new CreateWikiNotificationsManager(
+			$services->getDBLoadBalancerFactory(),
 			RequestContext::getMain(),
 			new ServiceOptions(
 				CreateWikiNotificationsManager::CONSTRUCTOR_OPTIONS,
