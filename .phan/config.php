0ce574a52ecd5a8307357a76f4471b3b3eec1da5
Lens0021 <lorentz0021@gmail.com>
2022-01-28 17:00:48 +0900

    doc: Add why suppress_issue_types is specified (#284)
    * doc: Add why suppress_issue_types is specified
    
    The words from https://github.com/miraheze/CreateWiki/pull/283#discussion_r794250715
    
    Co-authored-by: Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
    
    Co-authored-by: Universal Omega <54654040+Universal-Omega@users.noreply.github.com>

--- .phan/config.php
+++ .phan/config.php
@@ -14,6 +14,8 @@ $cfg['exclude_analysis_directory_list'] = array_merge(
 	]
 );
 
+// We explicitly specify this so that it
+// picks up on deprecations, and other issues
 $cfg['suppress_issue_types'] = [
 	'PhanTypeArraySuspiciousNullable',
 	'SecurityCheck-LikelyFalsePositive',
