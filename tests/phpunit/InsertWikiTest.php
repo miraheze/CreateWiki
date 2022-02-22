238755c84faa8a6a217f76bbd02dd7e67d573c9c
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-02-22 16:30:24 -0700

    Update InsertWikiTest.php

--- tests/phpunit/InsertWikiTest.php
+++ tests/phpunit/InsertWikiTest.php
@@ -13,14 +13,31 @@ class InsertWikiTest extends MediaWikiIntegrationTestCase {
 		$this->addDBData();
 	}
 
-	public function setUpBeforeClass(): void {
+	/* public static function setUpBeforeClass(): void {
 		parent::setUpBeforeClass();
-		$this->addDBData();
-	}
+		$dbw = wfGetDB( DB_PRIMARY );
 
-	public function dbPrefix() {
-		return '';
-	}
+		$dbw->insert(
+			'cw_wikis',
+			[
+				'wiki_dbname' => 'wikidb',
+				'wiki_dbcluster' => 'c1',
+				'wiki_sitename' => 'TestWiki',
+				'wiki_language' => 'en',
+				'wiki_private' => (int)0,
+				'wiki_creation' => $dbw->timestamp(),
+				'wiki_category' => 'uncategorised',
+				'wiki_closed' => (int)0,
+				'wiki_deleted' => (int)0,
+				'wiki_locked' => (int)0,
+				'wiki_inactive' => (int)0,
+				'wiki_inactive_exempt' => (int)0,
+				'wiki_url' => 'http://127.0.0.1:9412'
+			],
+			__METHOD__
+			// [ 'IGNORE' ]
+		);
+	} */
 
 	public function addDBData() {
 		parent::addDBData();
