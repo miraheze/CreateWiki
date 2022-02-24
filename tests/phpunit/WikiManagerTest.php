67f6e7fa3505c29d0d78063f2b02332fc7aebf72
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-02-24 16:24:24 -0700

    Do database drops; add grants for renamewikitest

--- tests/phpunit/WikiManagerTest.php
+++ tests/phpunit/WikiManagerTest.php
@@ -22,6 +22,7 @@ class WikiManagerTest extends MediaWikiIntegrationTestCase {
 
 		$db->begin();
 		$db->query( "GRANT ALL PRIVILEGES ON `createwikitest`.* TO 'wikiuser'@'localhost';" );
+		$db->query( "GRANT ALL PRIVILEGES ON `renamewikitest`.* TO 'wikiuser'@'localhost';" );
 		$db->query( "FLUSH PRIVILEGES;" );
 		$db->commit();
 	}
@@ -41,9 +42,15 @@ class WikiManagerTest extends MediaWikiIntegrationTestCase {
 	 * @covers ::rename
 	 */
 	public function testRename() {
+		$user = $this->getTestSysop()->getUser();
+		$wikiManager = new WikiManager( 'renamewikitest' );
+		$wikiManager->create( 'TestWiki', 'en', 0, 'uncategorised', $user->getName(), $user, 'Test' );
+
 		$wikiManager = new WikiManager( 'createwikitest' );
 
 		$this->assertNull( $wikiManager->rename( 'renamewikitest' ) );
+
+		$this->db->query( 'DROP DATABASE `createwikitest`;' );
 	}
 
 	/**
@@ -53,5 +60,7 @@ class WikiManagerTest extends MediaWikiIntegrationTestCase {
 		$wikiManager = new WikiManager( 'renamewikitest' );
 
 		$this->assertNull( $wikiManager->delete( true ) );
+
+		$this->db->query( 'DROP DATABASE `renamewikitest`;' );
 	}
 }
