e2c598e9eed77f300c17b68117ba8cf47b8e71dc
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-02-27 12:16:23 -0700

    Update WikiManagerTest.php

--- tests/phpunit/WikiManagerTest.php
+++ tests/phpunit/WikiManagerTest.php
@@ -143,12 +143,13 @@ class WikiManagerTest extends MediaWikiIntegrationTestCase {
 		$remoteWiki = new RemoteWiki( 'deletewikitest' );
 
 		$eligibleTimestamp = wfTimestamp( TS_MW, wfTimestamp( TS_UNIX, $remoteWiki->isDeleted() ) - ( 86400 * 8 ) );
-		$this->db->update( 'cw_wikis', [ 'wiki_deleted_timestamp' => $eligibleTimestamp ], [ 'wiki_dbname' => 'deletewikitest' ] );
+		$this->db->update( 'cw_wikis', [ 'wiki_deleted_timestamp' => $eligibleTimestamp ], [ 'wiki_dbname' => 'deletewikitest' ] )
+
+		$remoteWiki->delete();
 
 		$wikiManager = new WikiManager( 'deletewikitest' );
 		$this->assertSame( 'Wiki deletewikitest can not be deleted yet.', $wikiManager->delete() );
 
-		$remoteWiki->delete();
 		$remoteWiki->commit();
 
 		$this->assertTrue( (bool)$remoteWiki->isDeleted() );
