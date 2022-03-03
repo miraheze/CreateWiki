e630fdf79bfb74febcb799621e0d568e7220c5fc
lens0021 <lorentz0021@gmail.com>
2022-03-03 12:51:49 +0900

    Modify relative paths as the file was moved
    using the way I found on https://gerrit.wikimedia.org/g/mediawiki/extensions/AbuseFilter/+/900915eeb2e6375807f8948112ecf22117abaeeb/includes/Hooks/Handlers/SchemaChangesHandler.php#52

--- includes/Hooks/Handlers/Installer.php
+++ includes/Hooks/Handlers/Installer.php
@@ -8,121 +8,123 @@ class Installer implements LoadExtensionSchemaUpdatesHook {
 
 	/** @inheritDoc */
 	public function onLoadExtensionSchemaUpdates( $updater ) {
+		$dir = __DIR__ . "/../../../sql";
+
 		$updater->addExtensionTable(
 			'cw_requests',
-			__DIR__ . '/../sql/cw_requests.sql'
+			"$dir/cw_requests.sql"
 		);
 
 		$updater->addExtensionTable(
 			'cw_comments',
-			__DIR__ . '/../sql/cw_comments.sql'
+			"$dir/cw_comments.sql"
 		);
 
 		$updater->modifyExtensionField(
 			'cw_comments',
 			'cw_comment_user',
-			__DIR__ . '/../sql/patches/patch-cw_comments-int.sql'
+			"$dir/patches/patch-cw_comments-int.sql"
 		);
 
 		$updater->modifyExtensionField(
 			'cw_requests',
 			'cw_user',
-			__DIR__ . '/../sql/patches/patch-cw_requests-int.sql'
+			"$dir/patches/patch-cw_requests-int.sql"
 		);
 
 		$updater->modifyExtensionField(
 			'cw_comments',
 			'cw_comment_user',
-			__DIR__ . '/../sql/patches/patch-cw_comments-blob.sql'
+			"$dir/patches/patch-cw_comments-blob.sql"
 		);
 
 		$updater->addExtensionField(
 			'cw_requests',
 			'cw_bio',
-			__DIR__ . '/../sql/patches/patch-cw_requests-add-cw_bio.sql'
+			"$dir/patches/patch-cw_requests-add-cw_bio.sql"
 		);
 
 		$updater->addExtensionField(
 			'cw_wikis',
 			'wiki_inactive_exempt_reason',
-			__DIR__ . '/../sql/patches/patch-cw_wikis-add-wiki_inactive_exempt_reason.sql'
+			"$dir/patches/patch-cw_wikis-add-wiki_inactive_exempt_reason.sql"
 		);
 
 		$updater->addExtensionTable(
 			'cw_wikis',
-			__DIR__ . '/../sql/cw_wikis.sql'
+			"$dir/cw_wikis.sql"
 		);
 
 		$updater->addExtensionField(
 			'cw_wikis',
 			'wiki_deleted',
-			__DIR__ . '/../sql/patches/patch-deleted-wiki.sql'
+			"$dir/patches/patch-deleted-wiki.sql"
 		);
 
 		$updater->addExtensionField(
 			'cw_wikis',
 			'wiki_deleted_timestamp',
-			__DIR__ . '/../sql/patches/patch-deleted-wiki.sql'
+			"$dir/patches/patch-deleted-wiki.sql"
 		);
 
 		$updater->addExtensionField(
 			'cw_wikis',
 			'wiki_inactive_exempt',
-			__DIR__ . '/../sql/patches/patch-inactive-exempt.sql'
+			"$dir/patches/patch-inactive-exempt.sql"
 		);
 
 		$updater->addExtensionField(
 			'cw_wikis',
 			'wiki_locked',
-			__DIR__ . '/../sql/patches/patch-locked-wiki.sql'
+			"$dir/patches/patch-locked-wiki.sql"
 		);
 
 		$updater->addExtensionField(
 			'cw_wikis',
 			'wiki_url',
-			__DIR__ . '/../sql/patches/patch-domain-cols.sql'
+			"$dir/patches/patch-domain-cols.sql"
 		);
 
 		$updater->addExtensionIndex(
 			'cw_wikis',
 			'wiki_dbname',
-			__DIR__ . '/../sql/patches/patch-cw_wikis-add-indexes.sql'
+			"$dir/patches/patch-cw_wikis-add-indexes.sql"
 		);
 
 		$updater->addExtensionField(
 			'cw_wikis',
 			'wiki_experimental',
-			__DIR__ . '/../sql/patches/patch-cw_wikis-add-wiki_experimental.sql'
+			"$dir/patches/patch-cw_wikis-add-wiki_experimental.sql"
 		);
 
 		$updater->modifyExtensionField(
 			'cw_wikis',
 			'wiki_closed',
-			__DIR__ . '/../sql/patches/patch-cw_wikis-add-default-to-wiki_closed.sql'
+			"$dir/patches/patch-cw_wikis-add-default-to-wiki_closed.sql"
 		);
 
 		$updater->modifyExtensionField(
 			'cw_wikis',
 			'wiki_deleted',
-			__DIR__ . '/../sql/patches/patch-cw_wikis-add-default-to-wiki_deleted.sql'
+			"$dir/patches/patch-cw_wikis-add-default-to-wiki_deleted.sql"
 		);
 
 		$updater->modifyExtensionField(
 			'cw_wikis',
 			'wiki_inactive',
-			__DIR__ . '/../sql/patches/patch-cw_wikis-add-default-to-wiki_inactive.sql'
+			"$dir/patches/patch-cw_wikis-add-default-to-wiki_inactive.sql"
 		);
 
 		$updater->modifyExtensionField(
 			'cw_wikis',
 			'wiki_inactive_exempt',
-			__DIR__ . '/../sql/patches/patch-cw_wikis-add-default-to-wiki_inactive_exempt.sql'
+			"$dir/patches/patch-cw_wikis-add-default-to-wiki_inactive_exempt.sql"
 		);
 
 		$updater->modifyExtensionField(
 			'cw_wikis',
 			'wiki_locked',
-			__DIR__ . '/../sql/patches/patch-cw_wikis-add-default-to-wiki_locked.sql'
+			"$dir/patches/patch-cw_wikis-add-default-to-wiki_locked.sql"
 		);
 	}
 }
