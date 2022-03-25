bc10c82dd349692c6aa87f4d7898a25f3f60b1dc
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-03-25 12:25:05 -0600

    Create new CreateWikiRegexConstraint class

---
+++ includes/CreateWikiRegexConstraint.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Miraheze\CreateWiki;
+
+use StringUtils;
+
+class CreateWikiRegexConstraint {
+
+	/**
+	 * @param array $regexes
+	 * @return bool
+	 */
+	private static function validateRegexes( $regexes ) {
+		foreach ( $regexes as $regex ) {
+			if ( !StringUtils::isValidPCRERegex( $regex ) ) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+	/**
+	 * @param array $lines
+	 * @return array
+	 */
+	private static function cleanLines( $lines ) {
+		return array_filter(
+			array_map( 'trim',
+				preg_replace( '/#.*$/', '', $lines )
+			)
+		);
+	}
+
+	/**
+	 * @param array $lines
+	 * @return array
+	 */
+	private static function regexesFromText( $lines ) {
+		$lines = self::cleanLines( $lines );
+		$regexes = explode( "\n", $lines );
+
+		if ( self::validateRegexes( $regexes ) ) {
+			return $regexes;
+		}
+
+		return [];
+	}
+
+	/**
+	 * @param string $key
+	 * @return array
+	 */
+	public static function regexesFromMessage( $key ) {
+		$message = wfMessage( $key )->inContentLanguage();
+
+		if ( !$message->isDisabled() ) {
+			return self::regexesFromText( $message->plain() );
+		}
+
+		return [];
+	}
+}
