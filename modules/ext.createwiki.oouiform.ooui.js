d8ab423d3323021b8b935049574575b88a5ce8b4
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-06-29 12:54:27 -0600

    Update ext.createwiki.oouiform.ooui.js

--- modules/ext.createwiki.oouiform.ooui.js
+++ modules/ext.createwiki.oouiform.ooui.js
@@ -1,26 +1,23 @@
 ( function () {
 	$( function () {
-		var $baseform, tabs, wrapper, previousTab, switchingNoHash, $infuse;
+		var tabs, previousTab, switchingNoHash;
 
-		$baseform = $( '#baseform' );
+		tabs = OO.ui.infuse( $( '.createwiki-tabs' ) );
 
-		// Make sure the accessibility tip is selectable so that screen reader users take notice,
-		// but hide it by default to reduce visual clutter.
-		// Make sure it becomes visible when focused.
-		$( '<div>' ).addClass( 'mw-navigation-hint' )
-			.text( mw.msg( 'baseform-tabs-navigation-hint' ) )
-			.attr( 'tabIndex', 0 )
-			.prependTo( '#mw-content-text' );
-
-		tabs = new OO.ui.IndexLayout( {
-			expanded: false,
-			// Do not remove focus from the tabs menu after choosing a tab
-			autoFocus: false
-		} );
+		tabs.$element.addClass( 'createwiki-tabs-infused' );
 
 		function enhancePanel( panel ) {
+			var $infuse = $( panel.$element ).find( '.createwiki-infuse' );
+			$infuse.each( function () {
+				try {
+					OO.ui.infuse( this );
+				} catch ( error ) {
+					return;
+				}
+			} );
+
 			if ( !panel.$element.data( 'mw-section-infused' ) ) {
-				// mw-htmlform-autoinfuse-lazy class has been removed by replacing faketabs
+				panel.$element.removeClass( 'mw-htmlform-autoinfuse-lazy' );
 				mw.hook( 'htmlform.enhance' ).fire( panel.$element );
 				panel.$element.data( 'mw-section-infused', true );
 			}
@@ -38,7 +35,7 @@
 			// Changing the hash apparently causes keyboard focus to be lost?
 			// Save and restore it. This makes no sense though.
 			active = document.activeElement;
-			location.hash = '#mw-section-' + panel.getName();
+			location.hash = '#' + panel.getName();
 			if ( active ) {
 				active.focus();
 			}
@@ -49,11 +46,11 @@
 
 		/**
 		 * @ignore
-		 * @param {string} name the name of a tab without the prefix ("mw-section-")
+		 * @param {string} name The name of a tab
 		 * @param {boolean} [noHash] A hash will be set according to the current
 		 *  open section. Use this flag to suppress this.
 		 */
-		function switchBaseFormTab( name, noHash ) {
+		function switchCreateWikiTab( name, noHash ) {
 			if ( noHash ) {
 				switchingNoHash = true;
 			}
@@ -65,85 +62,47 @@
 		}
 
 		// Jump to correct section as indicated by the hash.
+		// This function is called onload and onhashchange.
 		function detectHash() {
 			var hash = location.hash,
 				matchedElement, $parentSection;
-			if ( hash.match( /^#mw-section-[\w]+$/ ) ) {
-				mw.storage.session.remove( 'mwbaseform-prevTab' );
-				switchBaseFormTab( hash.replace( '#mw-section-', '' ) );
+			if ( hash.match( /^#mw-section-[\w-]+$/ ) ) {
+				mw.storage.session.remove( 'createwiki-prevTab' );
+				switchCreateWikiTab( hash.slice( 1 ) );
 			} else if ( hash.match( /^#mw-[\w-]+$/ ) ) {
 				matchedElement = document.getElementById( hash.slice( 1 ) );
-				$parentSection = $( matchedElement ).parent().closest( '[id^="mw-section-"]' );
+				$parentSection = $( matchedElement ).closest( '.createwiki-section-fieldset' );
 				if ( $parentSection.length ) {
-					mw.storage.session.remove( 'mwbaseform-prevTab' );
+					mw.storage.session.remove( 'createwiki-prevTab' );
 					// Switch to proper tab and scroll to selected item.
-					switchBaseFormTab( $parentSection.attr( 'id' ).replace( 'mw-section-', '' ), true );
+					switchCreateWikiTab( $parentSection.attr( 'id' ), true );
 					matchedElement.scrollIntoView();
 				}
 			}
 		}
 
-		if ( mw.config.get( 'wgCreateWikiOOUIFormTabs' ) ) {
-			mw.config.get( 'wgCreateWikiOOUIFormTabs' ).forEach( function ( tabConfig ) {
-				var panel, $panelContents;
-
-				panel = new OO.ui.TabPanelLayout( tabConfig.name, {
-					expanded: false,
-					label: tabConfig.label
-				} );
-
-				$panelContents = $( '#mw-section-' + tabConfig.name );
-
-				// Hide the unnecessary PHP PanelLayouts
-				// (Do not use .remove(), as that would remove event handlers for everything inside them)
-				$panelContents.parent().detach();
-
-				panel.$element.append( $panelContents );
-				tabs.addTabPanels( [ panel ] );
-
-				// Remove duplicate labels
-				// (This must be after .addTabPanels(), otherwise the tab item doesn't exist yet)
-				$panelContents.children( 'legend' ).remove();
-				$panelContents.attr( 'aria-labelledby', panel.getTabItem().getElementId() );
-			} );
-
-			wrapper = new OO.ui.PanelLayout( {
-				expanded: false,
-				padded: false,
-				framed: true
-			} );
-
-			wrapper.$element.append( tabs.$element );
-			$baseform.prepend( wrapper.$element );
-
-			$( '.mw-baseform-faketabs' ).remove();
-
-			$( window ).on( 'load', function () {
-				var hash = location.hash;
-				if ( hash.match( /^#mw-[\w-]+/ ) ) {
-					detectHash();
-				} else if ( hash === '' ) {
-					switchBaseFormTab( 'other', true );
-				}
-			} );
-
-			// Restore the active tab after saving
-			previousTab = mw.storage.session.get( 'mwbaseform-prevTab' );
-			if ( previousTab ) {
-				switchBaseFormTab( previousTab, true );
-				// Deleting the key, the tab states should be reset until we press Save
-				mw.storage.session.remove( 'mwbaseform-prevTab' );
+		$( window ).on( 'hashchange', function () {
+			var hash = location.hash;
+			if ( hash.match( /^#mw-[\w-]+/ ) ) {
+				detectHash();
+			} else if ( hash === '' ) {
+				switchCreateWikiTab( $( '[id*=mw-section-]' ).attr( 'id' ), true );
 			}
+		} )
+			// Run the function immediately to select the proper tab on startup.
+			.trigger( 'hashchange' );
 
-			$( '#mw-baseform-form' ).on( 'submit', function () {
-				var value = tabs.getCurrentTabPanelName();
-				mw.storage.session.set( 'mwbaseform-prevTab', value );
-			} );
+		// Restore the active tab after saving
+		previousTab = mw.storage.session.get( 'createwiki-prevTab' );
+		if ( previousTab ) {
+			switchCreateWikiTab( previousTab, true );
+			// Deleting the key, the tab states should be reset until we press Save
+			mw.storage.session.remove( 'createwiki-prevTab' );
 		}
 
-		$infuse = $( document.body ).find( '[data-ooui*="createwiki-infuse"], .createwiki-infuse[name]' );
-		$infuse.each( function () {
-			OO.ui.infuse( this );
+		$( '#createwiki-form' ).on( 'submit', function () {
+			var value = tabs.getCurrentTabPanelName();
+			mw.storage.session.set( 'createwiki-prevTab', value );
 		} );
 	} );
 }() );
