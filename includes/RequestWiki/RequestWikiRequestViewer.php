f5b6173aece4c31351fffe43d0d234c64ac4087e
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-08-11 16:27:10 -0600

    Update RequestWikiRequestViewer.php

--- includes/RequestWiki/RequestWikiRequestViewer.php
+++ includes/RequestWiki/RequestWikiRequestViewer.php
@@ -377,7 +377,7 @@ class RequestWikiRequestViewer {
 			if ( $formData['submission-action'] == 'approve' ) {
 				$request->approve( $user, $formData['reason'] );
 			} elseif ( $formData['submission-action'] == 'onhold' ) {
-				$request->hold( $formData['reason'], $user );
+				$request->onhold( $formData['reason'], $user );
 			} else {
 				$request->decline( $formData['reason'], $user );
 			}
