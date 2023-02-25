c7d8a42a889e80840f7241648a3c9c4f4ab52437
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-02-25 13:23:55 -0700

    Update CreateWikiCreationHook.php

--- includes/Hooks/CreateWikiCreationHook.php
+++ includes/Hooks/CreateWikiCreationHook.php
@@ -2,11 +2,14 @@
 
 namespace Miraheze\CreateWiki\Hooks;
 
+use Wikimedia\Rdbms\DBConnRef;
+
 interface CreateWikiCreationHook {
 	/**
+	 * @param DBConnRef $cwdb
 	 * @param string $wiki dbname
 	 * @param bool $private
 	 * @return void
 	 */
-	public function onCreateWikiCreation( $wiki, $private ): void;
+	public function onCreateWikiCreation( $cwdb, $wiki, $private ): void;
 }
