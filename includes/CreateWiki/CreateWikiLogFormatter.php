72dc99edeb611370a35b295f5c6976e96e1d0748
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-04-15 13:49:16 -0600

    Suppress

--- includes/CreateWiki/CreateWikiLogFormatter.php
+++ includes/CreateWiki/CreateWikiLogFormatter.php
@@ -20,6 +20,7 @@ class CreateWikiLogFormatter extends LogFormatter {
 		$linkRenderer = MediaWikiServices::getInstance()->getLinkRenderer();
 
 		if ( $subtype === 'requestwiki' ) {
+			// @phan-suppress-next-line SecurityCheck-DoubleEscaped
 			$params[6] = str_replace( '#', '', $params[6] );
 
 			$params[6] = Message::rawParam( $linkRenderer->makeKnownLink(
