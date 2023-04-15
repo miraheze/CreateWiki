689d6e2b592de6cc576021d6f89d4b99a7d09708
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-04-15 13:52:56 -0600

    Fix suppress line

--- includes/CreateWiki/CreateWikiLogFormatter.php
+++ includes/CreateWiki/CreateWikiLogFormatter.php
@@ -20,9 +20,9 @@ class CreateWikiLogFormatter extends LogFormatter {
 		$linkRenderer = MediaWikiServices::getInstance()->getLinkRenderer();
 
 		if ( $subtype === 'requestwiki' ) {
-			// @phan-suppress-next-line SecurityCheck-DoubleEscaped
 			$params[6] = str_replace( '#', '', $params[6] );
 
+			// @phan-suppress-next-line SecurityCheck-DoubleEscaped
 			$params[6] = Message::rawParam( $linkRenderer->makeKnownLink(
 				Title::newFromText( SpecialPage::getTitleFor( 'RequestWikiQueue' ) . '/' . $params[6] ),
 				'#' . $params[6]
