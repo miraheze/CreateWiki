da2699cb97e69ddef8366f8029dc5bc10527e0f7
MacFan4000 <paulfkeffer@gmail.com>
2023-05-25 15:41:55 -0400

    Logging: respect $this->plaintext
    This will prevent raw html tags from showing up in IRC feeds, basically every other extension respects this parameter

--- includes/CreateWiki/CreateWikiLogFormatter.php
+++ includes/CreateWiki/CreateWikiLogFormatter.php
@@ -23,10 +23,18 @@ class CreateWikiLogFormatter extends LogFormatter {
 			$params[6] = str_replace( '#', '', $params[6] );
 
 			// @phan-suppress-next-line SecurityCheck-DoubleEscaped
-			$params[6] = Message::rawParam( $linkRenderer->makeKnownLink(
-				Title::newFromText( SpecialPage::getTitleFor( 'RequestWikiQueue' ) . '/' . $params[6] ),
-				'#' . $params[6]
-			) );
+			if ( !$this->plaintext ) {
+				$params[6] = Message::rawParam( $linkRenderer->makeKnownLink(
+					Title::newFromText( SpecialPage::getTitleFor( 'RequestWikiQueue' ) . '/' . $params[6] ),
+					'#' . $params[6]
+				) );
+			}
+			else {
+				$params[6] = Message::rawParam(
+					Title::newFromText( SpecialPage::getTitleFor( 'RequestWikiQueue' ) . '/' . $params[6] ),
+					'#' . $params[6]
+				);
+			}
 		}
 
 		return $params;
