d979064102fc6b1cd9f91abda46482a8ad5d9de8
paladox <paladox@users.noreply.github.com>
2023-09-17 17:27:17 +0100

    Update RemoteWikiTest.php

--- tests/phpunit/RemoteWikiTest.php
+++ tests/phpunit/RemoteWikiTest.php
@@ -66,7 +66,7 @@ class RemoteWikiTest extends MediaWikiIntegrationTestCase {
 				'user' => 'root',
 			] );
 		} else {
-			$db = Wikimedia\Rdbms\Database::factory( 'mysql', [ 'host' => $GLOBALS['wgDBserver'], 'user' => 'root' ] );
+			$db = \Wikimedia\Rdbms\Database::factory( 'mysql', [ 'host' => $GLOBALS['wgDBserver'], 'user' => 'root' ] );
 		}
 
 		$db->begin();
