585d912422184fb21451e3ea0bd85966345a8cd3
paladox <paladox@users.noreply.github.com>
2023-09-17 16:34:24 +0100

    Update RemoteWikiTest.php

--- tests/phpunit/RemoteWikiTest.php
+++ tests/phpunit/RemoteWikiTest.php
@@ -30,36 +30,34 @@ class RemoteWikiTest extends MediaWikiIntegrationTestCase {
 		$this->setMwGlobals( 'wgCreateWikiUseInactiveWikis', true );
 		$this->setMwGlobals( 'wgCreateWikiUsePrivateWikis', true );
 
-		if ( version_compare( MW_VERSION, '1.40', '>=' ) ) {
-			try {
-				$dbw = MediaWikiServices::getInstance()
-					->getDBLoadBalancer()
-					->getMaintenanceConnectionRef( DB_PRIMARY );
-	
-				$dbw->insert(
-					'cw_wikis',
-					[
-						'wiki_dbname' => 'wikidb',
-						'wiki_dbcluster' => 'c1',
-						'wiki_sitename' => 'TestWiki',
-						'wiki_language' => 'en',
-						'wiki_private' => (int)0,
-						'wiki_creation' => $dbw->timestamp(),
-						'wiki_category' => 'uncategorized',
-						'wiki_closed' => (int)0,
-						'wiki_deleted' => (int)0,
-						'wiki_locked' => (int)0,
-						'wiki_inactive' => (int)0,
-						'wiki_inactive_exempt' => (int)0,
-						'wiki_url' => 'http://127.0.0.1:9412'
-					],
-					__METHOD__,
-					[ 'IGNORE' ]
-				);
-	
-			} catch ( DBQueryError $e ) {
-				// Do nothing
-			}
+		try {
+			$dbw = MediaWikiServices::getInstance()
+				->getDBLoadBalancer()
+				->getMaintenanceConnectionRef( DB_PRIMARY );
+
+			$dbw->insert(
+				'cw_wikis',
+				[
+					'wiki_dbname' => 'wikidb',
+					'wiki_dbcluster' => 'c1',
+					'wiki_sitename' => 'TestWiki',
+					'wiki_language' => 'en',
+					'wiki_private' => (int)0,
+					'wiki_creation' => $dbw->timestamp(),
+					'wiki_category' => 'uncategorised',
+					'wiki_closed' => (int)0,
+					'wiki_deleted' => (int)0,
+					'wiki_locked' => (int)0,
+					'wiki_inactive' => (int)0,
+					'wiki_inactive_exempt' => (int)0,
+					'wiki_url' => 'http://127.0.0.1:9412'
+				],
+				__METHOD__,
+				[ 'IGNORE' ]
+			);
+
+		} catch ( DBQueryError $e ) {
+			// Do nothing
 		}
 
 		$db = MediaWikiServices::getInstance()->getDatabaseFactory()->create( 'mysql', [
