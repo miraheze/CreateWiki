69cdd764b8b9efae92f0c5ed0dbf8f494092d991
Reception123 <utilizator.receptie123@gmail.com>
2024-01-30 18:29:21 +0100

    add script to delete individual wikis
    Script to delete individual wikis rather than run the full deletion script when needed. Part of a planned script to reset wikis and also a good alternative to having to use eval.php. 
    
    There's probably some mistakes here.

---
+++ maintenance/deleteWiki.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Miraheze\CreateWiki\Maintenance;
+
+$IP = getenv('MW_INSTALL_PATH');
+if ($IP === false) {
+    $IP = __DIR__ . '/../../..';
+}
+
+require_once "$IP/maintenance/Maintenance.php";
+
+use Maintenance;
+use MediaWiki\MediaWikiServices;
+use Miraheze\CreateWiki\WikiManager;
+
+class DeleteWiki extends Maintenance {
+    public function __construct() {
+        parent::__construct();
+        $this->mDescription = "Deletes a single wiki. Does not drop databases";
+        $this->addOption( 'deletewiki', "Specify the database name to delete", false, true);
+        $this->addOption( 'delete', 'Actually performs deletions and not outputs the wiki to be deleted', false );
+    }
+
+    public function execute() {
+        $dbname = $this->getOption("deletewiki");
+
+        if (empty($dbname)) {
+            $this->output("Please specify the database to delete using the --deletewiki option.\n");
+            return;
+        }
+      
+        if ( $this->hasOption( 'delete' ) ) { 
+            $wm = new WikiManager($dbname);
+            $wm->delete(true);
+            $this->output("Wiki $dbname deleted.\n");
+        } 
+    }
+}
+
+// Run the maintenance script
+$maintClass = DeleteWiki::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
