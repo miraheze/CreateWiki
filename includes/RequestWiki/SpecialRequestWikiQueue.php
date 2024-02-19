4c8355300a0ee2125e2db45847f8d04badfc939e
Agent Isai <43097272+AgentIsai@users.noreply.github.com>
2024-02-18 22:50:25 -0600

    Make RequestWikiQueue use a legend

--- includes/RequestWiki/SpecialRequestWikiQueue.php
+++ includes/RequestWiki/SpecialRequestWikiQueue.php
@@ -35,6 +35,10 @@ class SpecialRequestWikiQueue extends SpecialPage {
 		$dbname = $this->getRequest()->getText( 'dbname' );
 
 		$formDescriptor = [
+			'intro' => [
+				'type' => 'info',
+				'default' => $this->msg( 'requestwikiqueue-info' )->text(),
+			],
 			'dbname' => [
 				'type' => 'text',
 				'name' => 'dbname',
@@ -64,6 +68,7 @@ class SpecialRequestWikiQueue extends SpecialPage {
 		];
 
 		$htmlForm = HTMLForm::factory( 'ooui', $formDescriptor, $this->getContext() );
+		$htmlForm->setWrapperLegendMsg( 'requestwikiqueue-search-header' );
 		$htmlForm->setMethod( 'get' )->prepareForm()->displayForm( false );
 
 		$pager = new RequestWikiQueuePager( $this, $requester, $dbname, $status );
