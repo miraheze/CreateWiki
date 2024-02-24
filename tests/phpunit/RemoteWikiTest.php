1cba7380353ca822ef6d7565fe3dc7ce62052c51
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-02-23 20:18:49 -0700

    Test

--- tests/phpunit/RemoteWikiTest.php
+++ tests/phpunit/RemoteWikiTest.php
@@ -13,6 +13,11 @@ use Wikimedia\Timestamp\ConvertibleTimestamp;
 
 define( 'CW_DB', 'wikidb' );
 
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../../..';
+}
+
 require_once "$IP/extensions/CreateWiki/includes/WikiInitialise.php";
 
 /**
