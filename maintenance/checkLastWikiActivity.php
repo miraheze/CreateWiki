79a148f9b3e1190452ce8be97e871707824590e0
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-02-27 13:01:08 -0700

    Comment for now

--- maintenance/checkLastWikiActivity.php
+++ maintenance/checkLastWikiActivity.php
@@ -27,11 +27,11 @@ class CheckLastWikiActivity extends Maintenance {
 	public function execute() {
 		$timestamp = $this->getTimestamp();
 		if ( $timestamp === 0 && SiteStats::edits() >= 2 ) {
-			$rebuildRC = $maintenance->runChild(
+			/* $rebuildRC = $maintenance->runChild(
 				RebuildRecentchanges::class,
 				MW_INSTALL_PATH . '/maintenance/rebuildrecentchanges.php'
-			);
-			$rebuildRC->execute();
+			) */;
+			// $rebuildRC->execute();
 			$timestamp = $this->getTimestamp();
 		}
 		$this->timestamp = $timestamp;
