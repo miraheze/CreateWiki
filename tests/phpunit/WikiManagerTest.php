88598c4f346f50d10fdac20f3478c0c77cba61e2
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-03-01 19:18:21 -0700

    Ensure wikidb is added

--- tests/phpunit/WikiManagerTest.php
+++ tests/phpunit/WikiManagerTest.php
@@ -44,6 +44,38 @@ class WikiManagerTest extends MediaWikiIntegrationTestCase {
 		$db->commit();
 	}
 
+	public function addDBDataOnce(): void {
+		try {
+			$dbw = MediaWikiServices::getInstance()
+				->getDBLoadBalancer()
+				->getMaintenanceConnectionRef( DB_PRIMARY );
+
+			$dbw->insert(
+				'cw_wikis',
+				[
+					'wiki_dbname' => 'wikidb',
+					'wiki_dbcluster' => 'c1',
+					'wiki_sitename' => 'TestWiki',
+					'wiki_language' => 'en',
+					'wiki_private' => (int)0,
+					'wiki_creation' => $dbw->timestamp(),
+					'wiki_category' => 'uncategorised',
+					'wiki_closed' => (int)0,
+					'wiki_deleted' => (int)0,
+					'wiki_locked' => (int)0,
+					'wiki_inactive' => (int)0,
+					'wiki_inactive_exempt' => (int)0,
+					'wiki_url' => 'http://127.0.0.1:9412'
+				],
+				__METHOD__,
+				[ 'IGNORE' ]
+			);
+
+		} catch ( DBQueryError $e ) {
+			// Do nothing
+		}
+	}
+
 	/**
 	 * @return CreateWikiHookRunner
 	 */
