d49e4db61cddc10f3a0056ec7116867b661bd2b7
Alex <alex@blueselene.com>
2024-03-25 23:03:00 +0100

    Fix CI

--- includes/RequestWiki/Handler/QueryWikiRequest.php
+++ includes/RequestWiki/Handler/QueryWikiRequest.php
@@ -90,6 +90,7 @@ class QueryWikiRequest extends SimpleHandler {
 			return $this->getResponseFactory()->createJson( $response );
 		}
 		// Request does not exist, or has been suppressed
+		// phpcs:ignore MediaWiki.WhiteSpace.SpaceyParenthesis.SingleSpaceBeforeCloseParenthesis
 		return $this->getResponseFactory()->createHttpError( 400, ['message' => 'Invalid request ID'] );
 	}
 
