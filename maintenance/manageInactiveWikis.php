18f9455b313a658b0e62ed5c1e7ca10bcb46550e
Alex <alex@blueselene.com>
2024-04-01 22:27:05 +0200

    fix CI (#502)

--- maintenance/manageInactiveWikis.php
+++ maintenance/manageInactiveWikis.php
@@ -143,14 +143,14 @@ class ManageInactiveWikis extends Maintenance {
 			if ( $wiki->isClosed() && $wiki->isClosed() < date( "YmdHis", strtotime( "-{$removeDays} days" ) ) ) {
 				// Wiki closed, eligible for deletion
 				if ( $canWrite ) {
-// $wiki->delete();
+					// $wiki->delete();
 
-//					$this->output( "{$dbName} is eligible to be removed from public viewing and has been.\n" );
-					$this->output( "Deletion is currently disabled until we make sure closure is happening as it should. Wiki is eligible but no action taken.\n" );
+					// $this->output( "{$dbName} is eligible to be removed from public viewing and has been.\n" );
+					$this->output( "Wiki is eligible for deletion, but deletion is currently disabled until we make sure closure is happening as it should.\n" );
 
 				} else {
-	// $this->output( "{$dbName} is eligible for public removal, was closed on {$wiki->isClosed()}.\n" );
-					$this->output( "Deletion is currently disabled until we make sure closure is happening as it should. Wiki is eligible but no action taken.\n" );
+					// $this->output( "{$dbName} is eligible for public removal, was closed on {$wiki->isClosed()}.\n" );
+					$this->output( "Wiki is eligible for deletion, and would've been deleted if this script was run with --write, but deletion is currently disabled until we make sure closure is happening as it should.\n" );
 				}
 			} elseif ( $wiki->isClosed() && $wiki->isClosed() > date( "YmdHis", strtotime( "-{$removeDays} days" ) ) ) {
 				// Wiki closed but not yet eligible for removal
