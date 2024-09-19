b63ba751f15f2b6596c96deb195b694c305a9408
Agent Isai <43097272+AgentIsai@users.noreply.github.com>
2024-09-19 00:04:03 -0500

    Make proteceted

--- includes/RequestWiki/WikiRequest.php
+++ includes/RequestWiki/WikiRequest.php
@@ -277,7 +277,7 @@ class WikiRequest {
 		$this->log( $user, 'requestmoredetails' );
 	}
 
-	private function log( UserIdentity $user, string $log ) {
+	protected function log( UserIdentity $user, string $log ) {
 		$logEntry = new ManualLogEntry( 'farmer', $log );
 		$logEntry->setPerformer( $user );
 		$logEntry->setTarget( SpecialPage::getTitleFor( 'RequestWikiQueue', $this->id ) );
