7f401e9f89dd33abdf945c51f388e416b9478f14
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-09-21 02:55:48 +0800

    Add hook

---
+++ includes/Hooks/CreateWikiPhpBuilderHook.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Miraheze\CreateWiki\Hooks;
+
+use Wikimedia\Rdbms\DBConnRef;
+
+interface CreateWikiPhpBuilderHook {
+	/**
+	 * @param string $wiki
+	 * @param DBConnRef $dbr
+	 * @param array &$cacheArray
+	 * @return void
+	 */
+	public function onCreateWikiPhpBuilder( $wiki, $dbr, &$cacheArray ): void;
+}
