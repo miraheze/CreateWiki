85000e6ed2eb772153d668431155fa5a45252e48
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-09-27 04:57:55 +0800

    Add service

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -5,6 +5,7 @@ use MediaWiki\MediaWikiServices;
 use Miraheze\CreateWiki\CreateWikiDataFactory;
 use Miraheze\CreateWiki\CreateWikiNotificationsManager;
 use Miraheze\CreateWiki\Hooks\CreateWikiHookRunner;
+use Miraheze\CreateWiki\RemoteWikiFactory;
 
 return [
 	'CreateWiki.NotificationsManager' => static function ( MediaWikiServices $services ): CreateWikiNotificationsManager {
@@ -31,5 +32,16 @@ return [
 	},
 	'CreateWikiHookRunner' => static function ( MediaWikiServices $services ): CreateWikiHookRunner {
 		return new CreateWikiHookRunner( $services->getHookContainer() );
+	},
+	'RemoteWikiFactory' => static function ( MediaWikiServices $services ): RemoteWikiFactory {
+		return new RemoteWikiFactory(
+			$services->getConnectionProvider(),
+			$services->get( 'CreateWikiDataFactory' ),
+			$services->get( 'CreateWikiHookRunner' ),
+			new ServiceOptions(
+				RemoteWikiFactory::CONSTRUCTOR_OPTIONS,
+				$services->getConfigFactory()->makeConfig( 'CreateWiki' )
+			)
+		);
 	},
 ];
