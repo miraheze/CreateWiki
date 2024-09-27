6b4abc9426a2df65e8728f90e12067a444271b88
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-09-28 03:44:33 +0800

    Remove wgConf

--- tests/phpunit/RemoteWikiFactoryTest.php
+++ tests/phpunit/RemoteWikiFactoryTest.php
@@ -2,7 +2,6 @@
 
 namespace Miraheze\CreateWiki\Tests;
 
-use MediaWiki\Config\SiteConfiguration;
 use MediaWikiIntegrationTestCase;
 use Miraheze\CreateWiki\Hooks\CreateWikiHookRunner;
 use Miraheze\CreateWiki\Services\RemoteWikiFactory;
@@ -21,10 +20,6 @@ class RemoteWikiFactoryTest extends MediaWikiIntegrationTestCase {
 	protected function setUp(): void {
 		parent::setUp();
 
-		$conf = new SiteConfiguration();
-		$conf->suffixes = [ 'test' ];
-
-		$this->setMwGlobals( 'wgConf', $conf );
 		$this->setMwGlobals( 'wgCreateWikiUseClosedWikis', true );
 		$this->setMwGlobals( 'wgCreateWikiUseExperimental', true );
 		$this->setMwGlobals( 'wgCreateWikiUseInactiveWikis', true );
