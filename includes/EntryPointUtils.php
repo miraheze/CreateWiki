5dc8f7252c36d9d5e82d22a1771b8ce2c544bf9d
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-09-27 10:38:56 +0800

    Use WikiMap::isCurrentWikiId and add TODO comment

--- includes/EntryPointUtils.php
+++ includes/EntryPointUtils.php
@@ -1,14 +1,17 @@
 <?php
+
 namespace Miraheze\CreateWiki;
 
 use MediaWiki\MediaWikiServices;
 use MediaWiki\WikiMap\WikiMap;
 
+// TODO: remove this class and use service injection
+// to get config in special pages directly
 class EntryPointUtils {
 
 	public static function currentWikiIsGlobalWiki(): bool {
 		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'CreateWiki' );
-		if ( $config->get( 'CreateWikiGlobalWiki' ) === WikiMap::getCurrentWikiId() ) {
+		if ( WikiMap::isCurrentWikiId( $config->get( 'CreateWikiGlobalWiki' ) ) ) {
 			return true;
 		} else {
 			return false;
