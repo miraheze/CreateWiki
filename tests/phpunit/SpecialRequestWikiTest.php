05eafaaa76205670a8c1316e3a3b033c6af256cd
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-09-27 12:55:08 +0800

    Split line

--- tests/phpunit/SpecialRequestWikiTest.php
+++ tests/phpunit/SpecialRequestWikiTest.php
@@ -104,7 +104,9 @@ class SpecialRequestWikiTest extends SpecialPageTestBase {
 
 		$specialRequestWiki->setContext( $testContext );
 		$this->expectException( ErrorPageError::class );
-		$this->expectExceptionMessageMatches( '/Your email is not confirmed. To request wikis, please \[\[Special:ChangeEmail\|confirm an email\]\] first./' );
+		$this->expectExceptionMessageMatches(
+			'/Your email is not confirmed. To request wikis, please \[\[Special:ChangeEmail\|confirm an email\]\] first./'
+		);
 
 		$specialRequestWiki->execute( '' );
 	}
