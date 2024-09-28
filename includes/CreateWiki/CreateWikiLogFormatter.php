98170a5fa8a94ed96ece5c105e42b4f7dfc769b8
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-09-29 03:23:59 +0800

    Use service injection

--- includes/CreateWiki/CreateWikiLogFormatter.php
+++ includes/CreateWiki/CreateWikiLogFormatter.php
@@ -2,27 +2,40 @@
 
 namespace Miraheze\CreateWiki\CreateWiki;
 
+use LogEntry;
 use LogFormatter;
-use MediaWiki\MediaWikiServices;
+use MediaWiki\Linker\LinkRenderer;
 use MediaWiki\Message\Message;
 use MediaWiki\SpecialPage\SpecialPage;
 use MediaWiki\Title\Title;
 
 class CreateWikiLogFormatter extends LogFormatter {
 
+	private LinkRenderer $linkRenderer;
+
 	/**
-	 * @return array
+	 * @param LogEntry $entry
+	 * @param LinkRenderer $linkRenderer
 	 */
-	protected function getMessageParameters() {
+	public function __construct(
+		LogEntry $entry,
+		LinkRenderer $linkRenderer
+	) {
+		parent::__construct( $entry );
+		$this->linkRenderer = $linkRenderer;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function getMessageParameters(): array {
 		$params = parent::getMessageParameters();
 		$subtype = $this->entry->getSubtype();
-
-		$linkRenderer = MediaWikiServices::getInstance()->getLinkRenderer();
 
 		if ( $subtype === 'requestwiki' ) {
 			$params[6] = str_replace( '#', '', $params[6] );
 
-			$params[6] = Message::rawParam( $linkRenderer->makeKnownLink(
+			$params[6] = Message::rawParam( $this->linkRenderer->makeKnownLink(
 				Title::newFromText( SpecialPage::getTitleFor( 'RequestWikiQueue' ) . '/' . $params[6] ),
 				'#' . $params[6]
 			) );
