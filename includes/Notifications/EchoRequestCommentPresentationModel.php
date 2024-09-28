e55142f970f8cb083b130907ed354eb141136adb
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-09-29 03:05:10 +0800

    Add type hints

--- includes/Notifications/EchoRequestCommentPresentationModel.php
+++ includes/Notifications/EchoRequestCommentPresentationModel.php
@@ -5,29 +5,45 @@ namespace Miraheze\CreateWiki\Notifications;
 use MediaWiki\Extension\Notifications\DiscussionParser;
 use MediaWiki\Extension\Notifications\Formatters\EchoEventPresentationModel;
 use MediaWiki\Language\RawMessage;
+use MediaWiki\Message\Message;
 
 class EchoRequestCommentPresentationModel extends EchoEventPresentationModel {
 
-	public function getIconType() {
+	/**
+	 * @inheritDoc
+	 */
+	public function getIconType(): string {
 		return 'chat';
 	}
 
-	public function getHeaderMessage() {
+	/**
+	 * @inheritDoc
+	 */
+	public function getHeaderMessage(): Message {
 		return $this->msg( 'notification-header-request-comment' );
 	}
 
-	public function getBodyMessage() {
+	/**
+	 * @inheritDoc
+	 */
+	public function getBodyMessage(): RawMessage {
 		$comment = $this->event->getExtraParam( 'comment' );
 		$text = DiscussionParser::getTextSnippet( $comment, $this->language );
 
 		return new RawMessage( '$1', [ $text ] );
 	}
 
-	public function getPrimaryLink() {
+	/**
+	 * @inheritDoc
+	 */
+	public function getPrimaryLink(): bool {
 		return false;
 	}
 
-	public function getSecondaryLinks() {
+	/**
+	 * @inheritDoc
+	 */
+	public function getSecondaryLinks(): array {
 		$visitLink = [
 			'url' => $this->event->getExtraParam( 'request-url', 0 ),
 			'label' => $this->msg( 'notification-createwiki-visit-request' )->text(),
