faf6229fba848bc725a9f25cc51cbca53ea4b70c
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-09-29 01:47:39 +0800

    Use service injection

--- includes/RequestWiki/RequestWikiAIJob.php
+++ includes/RequestWiki/RequestWikiAIJob.php
@@ -3,27 +3,44 @@
 namespace Miraheze\CreateWiki\RequestWiki;
 
 use Job;
-use MediaWiki\MediaWikiServices;
-use MediaWiki\Title\Title;
+use MediaWiki\Config\Config;
+use MediaWiki\Config\ConfigFactory;
 use MediaWiki\User\User;
 use Miraheze\CreateWiki\CreateWikiRegexConstraint;
+use Miraheze\CreateWiki\Hooks\CreateWikiHookRunner;
 use Phpml\ModelManager;
 
 class RequestWikiAIJob extends Job {
 
-	public function __construct( Title $title, array $params ) {
-		parent::__construct( 'RequestWikiAIJob', $params );
+	public const JOB_NAME = 'RequestWikiAIJob';
+
+	private Config $config;
+	private CreateWikiHookRunner $hookRunner;
+
+	private string $description;
+	private int $id;
+
+	public function __construct(
+		array $params,
+		ConfigFactory $configFactory,
+		CreateWikiHookRunner $hookRunner
+	) {
+		parent::__construct( self::JOB_NAME, $params );
+
+		$this->config = $configFactory->makeConfig( 'CreateWiki' );
+		$this->hookRunner = $hookRunner;
+
+		$this->description = $params['description'];
+		$this->id = $params['id'];
 	}
 
-	public function run() {
-		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'CreateWiki' );
-		$modelFile = $config->get( 'CreateWikiPersistentModelFile' );
-		$hookRunner = MediaWikiServices::getInstance()->get( 'CreateWikiHookRunner' );
+	public function run(): bool {
+		$modelFile = $this->config->get( 'CreateWikiPersistentModelFile' );
 
-		$wr = new WikiRequest( $this->params['id'] );
+		$wr = new WikiRequest( $this->id );
 
 		$pipeline = '';
-		$hookRunner->onCreateWikiReadPersistentModel( $pipeline );
+		$this->hookRunner->onCreateWikiReadPersistentModel( $pipeline );
 
 		// @phan-suppress-next-line PhanImpossibleCondition
 		if ( $pipeline || ( $modelFile && file_exists( $modelFile ) ) ) {
@@ -32,7 +49,7 @@ class RequestWikiAIJob extends Job {
 				$pipeline = $modelManager->restoreFromFile( $modelFile );
 			}
 
-			$tokenDescription = (array)strtolower( $this->params['description'] );
+			$tokenDescription = (array)strtolower( $this->description );
 
 			// @phan-suppress-next-line PhanUndeclaredMethod
 			$pipeline->transform( $tokenDescription );
@@ -40,9 +57,17 @@ class RequestWikiAIJob extends Job {
 			// @phan-suppress-next-line PhanUndeclaredMethod
 			$approveScore = $pipeline->getEstimator()->predictProbability( $tokenDescription )[0]['approved'];
 
-			$wr->addComment( 'Approval Score: ' . (string)round( $approveScore, 2 ), User::newSystemUser( 'CreateWiki Extension' ), false );
+			$wr->addComment(
+				'Approval Score: ' . (string)round( $approveScore, 2 ),
+				User::newSystemUser( 'CreateWiki Extension' ),
+				false
+			);
 
-			if ( is_int( $config->get( 'CreateWikiAIThreshold' ) ) && ( (int)round( $approveScore, 2 ) > $config->get( 'CreateWikiAIThreshold' ) ) && $this->canAutoApprove( $config ) ) {
+			if (
+				is_int( $this->config->get( 'CreateWikiAIThreshold' ) ) &&
+				( (int)round( $approveScore, 2 ) > $this->config->get( 'CreateWikiAIThreshold' ) ) &&
+				$this->canAutoApprove()
+			) {
 				$wr->approve( User::newSystemUser( 'CreateWiki Extension' ) );
 			}
 		}
@@ -50,13 +75,13 @@ class RequestWikiAIJob extends Job {
 		return true;
 	}
 
-	private function canAutoApprove( $config ) {
+	private function canAutoApprove(): bool {
 		$descriptionFilter = CreateWikiRegexConstraint::regexFromArray(
-			$config->get( 'CreateWikiAutoApprovalFilter' ), '/(', ')+/',
+			$this->config->get( 'CreateWikiAutoApprovalFilter' ), '/(', ')+/',
 			'CreateWikiAutoApprovalFilter'
 		);
 
-		if ( preg_match( $descriptionFilter, strtolower( $this->params['description'] ) ) ) {
+		if ( preg_match( $descriptionFilter, strtolower( $this->description ) ) ) {
 			return false;
 		}
 
