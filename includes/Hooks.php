1ac4e0cc43407a5fa38285f9b4b4cae5d6d7bcd2
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-09-29 03:56:00 +0800

    Move Echo hook to own handler

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -4,8 +4,6 @@ namespace Miraheze\CreateWiki;
 
 use MediaWiki\Config\Config;
 use MediaWiki\Config\ConfigFactory;
-use MediaWiki\Extension\Notifications\AttributeManager;
-use MediaWiki\Extension\Notifications\UserLocator;
 use MediaWiki\Hook\GetMagicVariableIDsHook;
 use MediaWiki\Hook\LoginFormValidErrorMessagesHook;
 use MediaWiki\Hook\ParserGetVariableValueSwitchHook;
@@ -13,10 +11,6 @@ use MediaWiki\Hook\SetupAfterCacheHook;
 use MediaWiki\MainConfigNames;
 use MediaWiki\Output\Hook\MakeGlobalVariablesScriptHook;
 use MediaWiki\Title\Title;
-use Miraheze\CreateWiki\Notifications\EchoCreateWikiPresentationModel;
-use Miraheze\CreateWiki\Notifications\EchoRequestCommentPresentationModel;
-use Miraheze\CreateWiki\Notifications\EchoRequestDeclinedPresentationModel;
-use Miraheze\CreateWiki\Notifications\EchoRequestMoreDetailsPresentationModel;
 use Miraheze\CreateWiki\Services\CreateWikiDataFactory;
 use Miraheze\CreateWiki\Services\RemoteWikiFactory;
 use Wikimedia\Rdbms\IConnectionProvider;
@@ -118,89 +112,5 @@ class Hooks implements
 	/** @inheritDoc */
 	public function onGetMagicVariableIDs( &$variableIDs ) {
 		$variableIDs[] = 'numberofwikirequests';
-	}
-
-	/**
-	 * Add CreateWiki events to Echo
-	 *
-	 * @param array &$notifications array of Echo notifications
-	 * @param array &$notificationCategories array of Echo notification categories
-	 * @param array &$icons array of icon details
-	 */
-	public static function onBeforeCreateEchoEvent(
-		&$notifications, &$notificationCategories, &$icons
-	) {
-		$notificationCategories['wiki-creation'] = [
-			'priority' => 3,
-			'no-dismiss' => [ 'all' ]
-		];
-
-		$notificationCategories['request-declined'] = [
-			'priority' => 3,
-			'tooltip' => 'echo-pref-tooltip-wiki-request-declined',
-			'no-dismiss' => [ 'email' ]
-		];
-
-		$notificationCategories['request-moredetails'] = [
-			'priority' => 1,
-			'no-dismiss' => [ 'all' ]
-		];
-
-		$notificationCategories['request-comment'] = [
-			'priority' => 3,
-			'tooltip' => 'echo-pref-tooltip-wiki-request-comment'
-		];
-
-		$notifications['wiki-creation'] = [
-			AttributeManager::ATTR_LOCATORS => [
-				[ [ UserLocator::class, 'locateEventAgent' ] ],
-			],
-			'category' => 'wiki-creation',
-			'group' => 'positive',
-			'section' => 'alert',
-			'canNotifyAgent' => true,
-			'presentation-model' => EchoCreateWikiPresentationModel::class,
-			'immediate' => true
-		];
-
-		$notifications['request-declined'] = [
-			AttributeManager::ATTR_LOCATORS => [
-				[ [ UserLocator::class, 'locateEventAgent' ] ],
-			],
-			'category' => 'request-declined',
-			'group' => 'positive',
-			'section' => 'alert',
-			'canNotifyAgent' => true,
-			'presentation-model' => EchoRequestDeclinedPresentationModel::class,
-			'immediate' => true
-		];
-
-		$notifications['request-moredetails'] = [
-			AttributeManager::ATTR_LOCATORS => [
-				[ [ UserLocator::class, 'locateEventAgent' ] ],
-			],
-			'category' => 'request-moredetails',
-			'group' => 'positive',
-			'section' => 'alert',
-			'canNotifyAgent' => true,
-			'presentation-model' => EchoRequestMoreDetailsPresentationModel::class,
-			'immediate' => true
-		];
-
-		$notifications['request-comment'] = [
-			AttributeManager::ATTR_LOCATORS => [
-				[ [ UserLocator::class, 'locateEventAgent' ] ],
-			],
-			'category' => 'request-comment',
-			'group' => 'positive',
-			'section' => 'alert',
-			'canNotifyAgent' => true,
-			'presentation-model' => EchoRequestCommentPresentationModel::class,
-			'immediate' => true
-		];
-
-		$icons['request-declined'] = [
-			'path' => 'CreateWiki/modules/icons/decline.svg'
-		];
 	}
 }
