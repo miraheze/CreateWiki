2a54347221338026f1f979ec18f6352282e8fbf6
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-09-30 12:52:31 +0800

    Add ConfigNames class

---
+++ includes/ConfigNames.php
@@ -0,0 +1,77 @@
+<?php
+
+// phpcs:disable Generic.NamingConventions.UpperCaseConstantName.ClassConstantNotUpperCase
+namespace Miraheze\CreateWiki;
+
+/**
+ * A class containing constants representing the names of configuration variables,
+ * to protect against typos.
+ */
+class ConfigNames {
+
+	public const AIThreshold = 'CreateWikiAIThreshold';
+
+	public const AutoApprovalFilter = 'CreateWikiAutoApprovalFilter';
+
+	public const CacheDirectory = 'CreateWikiCacheDirectory';
+
+	public const CacheType = 'CreateWikiCacheType';
+
+	public const CannedResponses = 'CreateWikiCannedResponses';
+
+	public const Categories = 'CreateWikiCategories';
+
+	public const Collation = 'CreateWikiCollation';
+
+	public const Containers = 'CreateWikiContainers';
+
+	public const CustomDomainPage = 'CreateWikiCustomDomainPage';
+
+	public const Database = 'CreateWikiDatabase';
+
+	public const DatabaseClusters = 'CreateWikiDatabaseClusters';
+
+	public const DatabaseClustersInactive = 'CreateWikiDatabaseClustersInactive';
+
+	public const DatabaseSuffix = 'CreateWikiDatabaseSuffix';
+
+	public const DisableRESTAPI = 'CreateWikiDisableRESTAPI';
+
+	public const DisallowedSubdomains = 'CreateWikiDisallowedSubdomains';
+
+	public const EmailNotifications = 'CreateWikiEmailNotifications';
+
+	public const EnableManageInactiveWikis = 'CreateWikiEnableManageInactiveWikis';
+
+	public const GlobalWiki = 'CreateWikiGlobalWiki';
+
+	public const InactiveExemptReasonOptions = 'CreateWikiInactiveExemptReasonOptions';
+
+	public const NotificationEmail = 'CreateWikiNotificationEmail';
+
+	public const PersistentModelFile = 'CreateWikiPersistentModelFile';
+
+	public const Purposes = 'CreateWikiPurposes';
+
+	public const ShowBiographicalOption = 'CreateWikiShowBiographicalOption';
+
+	public const SQLFiles = 'CreateWikiSQLfiles';
+
+	public const StateDays = 'CreateWikiStateDays';
+
+	public const Subdomain = 'CreateWikiSubdomain';
+
+	public const UseClosedWikis = 'CreateWikiUseClosedWikis';
+
+	public const UseCustomDomains = 'CreateWikiUseCustomDomains';
+
+	public const UseEchoNotifications = 'CreateWikiUseEchoNotifications';
+
+	public const UseExperimental = 'CreateWikiUseExperimental';
+
+	public const UseInactiveWikis = 'CreateWikiUseInactiveWikis';
+
+	public const UseJobQueue = 'CreateWikiUseJobQueue';
+
+	public const UsePrivateWikis = 'CreateWikiUsePrivateWikis';
+}
