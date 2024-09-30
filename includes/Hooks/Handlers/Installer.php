601b7b3a5fd7da813f33a52f37fb6c419e7b2a62
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-09-30 10:07:43 +0800

    Add patches

--- includes/Hooks/Handlers/Installer.php
+++ includes/Hooks/Handlers/Installer.php
@@ -20,111 +20,26 @@ class Installer implements LoadExtensionSchemaUpdatesHook {
 			"$dir/cw_comments.sql"
 		);
 
-		$updater->modifyExtensionField(
-			'cw_comments',
-			'cw_comment_user',
-			"$dir/patches/patch-cw_comments-int.sql"
-		);
-
-		$updater->modifyExtensionField(
-			'cw_requests',
-			'cw_user',
-			"$dir/patches/patch-cw_requests-int.sql"
-		);
-
-		$updater->modifyExtensionField(
-			'cw_comments',
-			'cw_comment_user',
-			"$dir/patches/patch-cw_comments-blob.sql"
-		);
-
-		$updater->addExtensionField(
-			'cw_requests',
-			'cw_bio',
-			"$dir/patches/patch-cw_requests-add-cw_bio.sql"
-		);
-
-		$updater->addExtensionField(
-			'cw_wikis',
-			'wiki_inactive_exempt_reason',
-			"$dir/patches/patch-cw_wikis-add-wiki_inactive_exempt_reason.sql"
-		);
-
 		$updater->addExtensionTable(
 			'cw_wikis',
 			"$dir/cw_wikis.sql"
 		);
 
 		$updater->addExtensionField(
-			'cw_wikis',
-			'wiki_deleted',
-			"$dir/patches/patch-deleted-wiki.sql"
+			'cw_requests',
+			'cw_locked',
+			"$dir/patches/patch-cw_requests-add-cw_locked.sql"
 		);
 
 		$updater->addExtensionField(
-			'cw_wikis',
-			'wiki_deleted_timestamp',
-			"$dir/patches/patch-deleted-wiki.sql"
+			'cw_requests',
+			'cw_extra',
+			"$dir/patches/patch-cw_requests-add-cw_extra.sql"
 		);
 
-		$updater->addExtensionField(
+		$updater->modifyExtensionTable(
 			'cw_wikis',
-			'wiki_inactive_exempt',
-			"$dir/patches/patch-inactive-exempt.sql"
-		);
-
-		$updater->addExtensionField(
-			'cw_wikis',
-			'wiki_locked',
-			"$dir/patches/patch-locked-wiki.sql"
-		);
-
-		$updater->addExtensionField(
-			'cw_wikis',
-			'wiki_url',
-			"$dir/patches/patch-domain-cols.sql"
-		);
-
-		$updater->addExtensionIndex(
-			'cw_wikis',
-			'wiki_dbname',
-			"$dir/patches/patch-cw_wikis-add-indexes.sql"
-		);
-
-		$updater->addExtensionField(
-			'cw_wikis',
-			'wiki_experimental',
-			"$dir/patches/patch-cw_wikis-add-wiki_experimental.sql"
-		);
-
-		$updater->modifyExtensionField(
-			'cw_wikis',
-			'wiki_closed',
-			"$dir/patches/patch-cw_wikis-add-default-to-wiki_closed.sql"
-		);
-
-		$updater->modifyExtensionField(
-			'cw_wikis',
-			'wiki_deleted',
-			"$dir/patches/patch-cw_wikis-add-default-to-wiki_deleted.sql"
-		);
-
-		$updater->modifyExtensionField(
-			'cw_wikis',
-			'wiki_inactive',
-			"$dir/patches/patch-cw_wikis-add-default-to-wiki_inactive.sql"
-		);
-
-		$updater->modifyExtensionField(
-			'cw_wikis',
-			'wiki_inactive_exempt',
-			"$dir/patches/patch-cw_wikis-add-default-to-wiki_inactive_exempt.sql"
-		);
-
-		$updater->modifyExtensionField(
-			'cw_wikis',
-			'wiki_locked',
-			"$dir/patches/patch-cw_wikis-add-default-to-wiki_locked.sql"
+			"$dir/patches/patch-cw_wikis-update-smallint-to-tinyint.sql"
 		);
 	}
 }
