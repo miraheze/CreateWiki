515ab8af478da54fa23c0d1c8e8e5e8281e506b9
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-09-30 10:31:44 +0800

    Inject config into RestUtils

--- includes/RestUtils.php
+++ includes/RestUtils.php
@@ -2,7 +2,7 @@
 
 namespace Miraheze\CreateWiki;
 
-use MediaWiki\MediaWikiServices;
+use MediaWiki\Config\Config;
 use MediaWiki\Rest\LocalizedHttpException;
 use MediaWiki\WikiMap\WikiMap;
 use Wikimedia\Message\MessageValue;
@@ -12,9 +12,7 @@ class RestUtils {
 	/**
 	 * Called from the REST handlers, checks that the current wiki is the global wiki and that the REST API is not disabled
 	 */
-	public static function checkEnv() {
-		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'CreateWiki' );
-
+	public static function checkEnv( Config $config ): void {
 		if ( !WikiMap::isCurrentWikiId( $config->get( 'CreateWikiGlobalWiki' ) ) ) {
 			throw new LocalizedHttpException( new MessageValue( 'createwiki-wikinotglobalwiki' ), 403 );
 		}
