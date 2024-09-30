db836da9a19b610066bb67b441acb9e84f65fa39
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-10-01 07:39:46 +0800

    Test class history

---
+++ includes/RequestWiki/RequestWikiHistory.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Miraheze\CreateWiki\RequestWiki;
+
+use OOUI\FieldsetLayout;
+use OOUI\LabelElement;
+use OOUI\PanelLayout;
+
+class RequestWikiHistory {
+
+	public function showHistorySection(array $historyEntries, $out) {
+
+		$historyFieldset = new FieldsetLayout([
+			'label' => 'History',
+			'items' => [
+				new LabelElement([
+					'label' => 'History of actions',
+				]),
+				$this->createHistoryTable($historyEntries),
+			]
+		]);
+
+		// Add the fieldset to your output
+		$out->addHTML($historyFieldset->toHTML());
+	}
+
+	private function createHistoryTable($entries) {
+		// Start building the HTML table
+		$tableHTML = '<table class="oo-ui-table oo-ui-table-striped">';
+		$tableHTML .= '<thead><tr>';
+		$tableHTML .= '<th>Timestamp</th>';
+		$tableHTML .= '<th>User</th>';
+		$tableHTML .= '<th>Action</th>';
+		$tableHTML .= '<th>Details</th>';
+		$tableHTML .= '</tr></thead>';
+		$tableHTML .= '<tbody>';
+
+		// Loop through each entry and create table rows
+		foreach ($entries as $entry) {
+			$tableHTML .= '<tr>';
+			$tableHTML .= '<td>' . htmlspecialchars($this->context->getLanguage()->timeanddate($entry['timestamp'], true)) . '</td>';
+			$tableHTML .= '<td>' . htmlspecialchars($entry['user']->getName()) . '</td>';
+			$tableHTML .= '<td>' . htmlspecialchars($entry['action']) . '</td>';
+			$tableHTML .= '<td>' . htmlspecialchars($entry['details']) . '</td>';
+			$tableHTML .= '</tr>';
+		}
+
+		$tableHTML .= '</tbody></table>';
+
+		// Return the completed table HTML
+		return $tableHTML;
+	}
+}
