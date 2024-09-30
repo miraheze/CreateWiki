f6496faee110028cfcb7d2732eac37b08ef178ef
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-09-30 09:56:41 +0800

    Remove very old patches

--- sql/patches/patch-deleted-wiki.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE /*$wgDBprefix*/cw_wikis
-  ADD COLUMN wiki_deleted TINYINT NOT NULL DEFAULT '0' AFTER wiki_closed_timestamp,
-  ADD COLUMN wiki_deleted_timestamp BINARY(14) NULL AFTER wiki_deleted;
