1f19c0a83964ca0a9abb8cfeb01eae53aaf3d847
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-10-09 02:24:57 +0800

    Use RuntimeException if command line mode

--- includes/Exceptions/MissingWikiError.php
+++ includes/Exceptions/MissingWikiError.php
@@ -5,10 +5,17 @@ namespace Miraheze\CreateWiki\Exceptions;
 use ErrorPageError;
 use MediaWiki\Html\Html;
 use MediaWiki\Language\RawMessage;
+use RuntimeException;
 
 class MissingWikiError extends ErrorPageError {
 
 	public function __construct( string $msg, array $params ) {
+		if ( !self::isCommandLine() ) {
+			throw new RuntimeException(
+				wfMessage( $msg, $params )->inContentLanguage()->escaped()
+			);
+		}
+			
 		$errorBody = new RawMessage( Html::errorBox( wfMessage( $msg, $params )->parse() ) );
 		parent::__construct( 'errorpagetitle', $errorBody, [] );
 	}
