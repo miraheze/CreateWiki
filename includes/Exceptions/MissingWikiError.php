9507d0011ad670dfbb5c45bffad711fe0b1ba68b
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-10-11 06:46:01 +0800

    Fix

--- includes/Exceptions/MissingWikiError.php
+++ includes/Exceptions/MissingWikiError.php
@@ -5,15 +5,17 @@ namespace Miraheze\CreateWiki\Exceptions;
 use ErrorPageError;
 use MediaWiki\Html\Html;
 use MediaWiki\Language\RawMessage;
+use MWExceptionRenderer;
 
 class MissingWikiError extends ErrorPageError {
 
 	public function __construct( string $msg, array $params ) {
 		$errorBody = new RawMessage( Html::errorBox( wfMessage( $msg, $params )->parse() ) );
 		if ( self::isCommandLine() ) {
-			$errorBody = wfMessage( $msg, $params )->inContentLanguage()->text();
+			$fallback = wfMessage( $msg, $params )->inContentLanguage()->text();
+			$errorBody = new RawMessage( MWExceptionRenderer::msg( $msg, $fallback, $params ) );
 		}
-			
+
 		parent::__construct( 'errorpagetitle', $errorBody, [] );
 	}
 }
