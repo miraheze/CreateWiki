db8b761829de7705610f6b9275b8dfeae41649b3
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-10-16 14:19:30 -0600

    Add DetailsWithIconField

---
+++ includes/RequestWiki/FormFields/DetailsWithIconField.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Miraheze\CreateWiki\RequestWiki\FormFields;
+
+use MediaWiki\HTMLForm\HTMLFormField;
+use OOUI\IconWidget;
+use OOUI\Tag;
+
+class DetailsWithIconField extends HTMLFormField {
+
+	private bool $fieldCheck;
+
+	/** @inheritDoc */
+	public function __construct( $info ) {
+		$this->fieldCheck = $info['fieldCheck'] ?? false;
+		parent::__construct( $info );
+	}
+
+	/** @inheritDoc */
+	public function getInputOOUI( $value ): string {
+		if ( $this->fieldCheck ) {
+			$icon = new IconWidget( [
+				'icon' => 'check',
+				'flags' => [ 'success' ],
+			] );
+
+			$text = ( new Tag( 'b' ) )->appendContent(
+				$this->msg( 'htmlform-yes' )->escaped()
+			);
+		} else {
+			$icon = new IconWidget( [
+				'icon' => 'close',
+				'flags' => [ 'progressive' ],
+			] );
+
+			$text = ( new Tag( 'b' ) )->appendContent(
+				$this->msg( 'htmlform-no' )->escaped()
+			);
+		}
+
+		return $icon . ' ' . $text;
+	}
+}
