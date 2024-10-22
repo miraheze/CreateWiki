f6499338a64d9e349e0568b1fb80ce8af54c36f7
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-10-22 14:33:23 -0600

    -

--- includes/RequestWiki/Specials/SpecialFlaggedRequests.php
+++ includes/RequestWiki/Specials/SpecialFlaggedRequests.php
@@ -4,46 +4,30 @@ namespace Miraheze\CreateWiki\RequestWiki\Specials;
 
 use ErrorPageError;
 use MediaWiki\HTMLForm\HTMLForm;
-use MediaWiki\Languages\LanguageNameUtils;
 use MediaWiki\Permissions\PermissionManager;
 use MediaWiki\SpecialPage\SpecialPage;
 use MediaWiki\User\UserFactory;
 use MediaWiki\WikiMap\WikiMap;
 use Mirabeze\CreateWiki\RequestWiki\FlaggedRequestsPager;
 use Miraheze\CreateWiki\ConfigNames;
-use Miraheze\CreateWiki\Hooks\CreateWikiHookRunner;
-use Miraheze\CreateWiki\Services\WikiManagerFactory;
-use Miraheze\CreateWiki\Services\WikiRequestManager;
 use Wikimedia\Rdbms\IConnectionProvider;
 
 class SpecialFlaggedRequests extends SpecialPage {
 
 	private IConnectionProvider $connectionProvider;
-	private CreateWikiHookRunner $hookRunner;
-	private LanguageNameUtils $languageNameUtils;
 	private PermissionManager $permissionManager;
 	private UserFactory $userFactory;
-	private WikiManagerFactory $wikiManagerFactory;
-	private WikiRequestManager $wikiRequestManager;
 
 	public function __construct(
 		IConnectionProvider $connectionProvider,
-		CreateWikiHookRunner $hookRunner,
-		LanguageNameUtils $languageNameUtils,
 		PermissionManager $permissionManager,
-		UserFactory $userFactory,
-		WikiManagerFactory $wikiManagerFactory,
-		WikiRequestManager $wikiRequestManager
+		UserFactory $userFactory
 	) {
 		parent::__construct( 'RequestWikiQueue', 'createwiki' );
 
 		$this->connectionProvider = $connectionProvider;
-		$this->hookRunner = $hookRunner;
-		$this->languageNameUtils = $languageNameUtils;
 		$this->permissionManager = $permissionManager;
 		$this->userFactory = $userFactory;
-		$this->wikiManagerFactory = $wikiManagerFactory;
-		$this->wikiRequestManager = $wikiRequestManager;
 	}
 
 	/**
