e4ea7555a058adc0bf3690e1979b261a9df23eb1
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-10-27 11:08:52 -0600

    Fix

--- maintenance/manageInactiveWikisV2.php
+++ maintenance/manageInactiveWikisV2.php
@@ -151,7 +151,7 @@ class ManageInactiveWikisV2 extends Maintenance {
 		$isClosed = $remoteWiki->isClosed() && $remoteWiki->getClosedTimestamp();
 		if ( $isClosed && $remoteWiki->getClosedTimestamp() < date( 'YmdHis', strtotime( "-{$removeDays} days" ) ) ) {
 			if ( $canWrite ) {
-				$remoteWiki->delete( force: false );
+				$remoteWiki->delete();
 				$this->output( "{$dbName} is eligible for removal and now has been.\n" );
 			} else {
 				$this->output(
