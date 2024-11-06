9b277db2786c2d634909bd4d8ef3928fc2e64658
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-11-05 19:07:23 -0700

    Add CreateWikiDatabaseUtils

---
+++ includes/Services/CreateWikiDatabaseUtils.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Miraheze\CreateWiki\Services;
+
+use Wikimedia\Rdbms\IConnectionProvider;
+use Wikimedia\Rdbms\IDatabase;
+use Wikimedia\Rdbms\IReadableDatabase;
+
+class CreateWikiDatabaseUtils {
+
+	private IConnectionProvider $connectionProvider;
+
+	public function __construct( IConnectionProvider $connectionProvider ) {
+		$this->connectionProvider = $connectionProvider;
+	}
+
+	public function getCreateWikiPrimaryDB(): IDatabase {
+		return $this->connectionProvider->getPrimaryDatabase( 'virtual-createwiki' );
+	}
+
+	public function getCreateWikiReplicaDB(): IReadableDatabase {
+		return $this->connectionProvider->getReplicaDatabase( 'virtual-createwiki' );
+	}
+
+	private function getGlobalWikiID(): bool|string {
+		return $this->getGlobalWikiReplicaDB()->getDomainID();
+	}
+
+	public function getGlobalWikiPrimaryDB(): IDatabase {
+		return $this->connectionProvider->getPrimaryDatabase( 'virtual-createwiki-global' );
+	}
+
+	public function getGlobalWikiReplicaDB(): IReadableDatabase {
+		return $this->connectionProvider->getReplicaDatabase( 'virtual-createwiki-global' );
+	}
+}
