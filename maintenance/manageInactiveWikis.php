46e3d67d82333aab9f12457728f77551c7d1170c
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-11-19 13:50:23 -0700

    Use ConnectionProvider

--- maintenance/manageInactiveWikis.php
+++ maintenance/manageInactiveWikis.php
@@ -36,7 +36,8 @@ class ManageInactiveWikis extends Maintenance {
 		}
 
 		$remoteWikiFactory = $this->getServiceContainer()->get( 'RemoteWikiFactory' );
-		$dbr = $this->getDB( DB_REPLICA, [], 'virtual-createwiki' );
+		$connectionProvider = $this->getServiceContainer()->getConnectionProvider();
+		$dbr = $connectionProvider->getReplicaDatabase( 'virtual-createwiki' );
 
 		$res = $dbr->newSelectQueryBuilder()
 			->select( 'wiki_dbname' )
