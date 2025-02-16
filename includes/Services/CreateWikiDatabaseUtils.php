391502a5b5f36beee1a5ed98437bd234c84aef7a
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-02-16 14:29:43 -0700

    Expand usage of CreateWikiDatabaseUtils

--- includes/Services/CreateWikiDatabaseUtils.php
+++ includes/Services/CreateWikiDatabaseUtils.php
@@ -4,6 +4,8 @@ namespace Miraheze\CreateWiki\Services;
 
 use MediaWiki\WikiMap\WikiMap;
 use Wikimedia\Rdbms\IConnectionProvider;
+use Wikimedia\Rdbms\IDatabase;
+use Wikimedia\Rdbms\IReadableDatabase;
 
 class CreateWikiDatabaseUtils {
 
@@ -13,11 +15,30 @@ class CreateWikiDatabaseUtils {
 	}
 
 	public function getCentralWikiID(): bool|string {
-		$dbr = $this->connectionProvider->getReplicaDatabase( 'virtual-createwiki-central' );
-		return $dbr->getDomainID();
+		return $this->getCentralWikiReplicaDB()->getDomainID();
+	}
+
+	public function getCentralWikiPrimaryDB(): IDatabase {
+		return $this->connectionProvider->getPrimaryDatabase( 'virtual-createwiki-central' );
+	}
+
+	public function getCentralWikiReplicaDB(): IReadableDatabase {
+		return $this->connectionProvider->getReplicaDatabase( 'virtual-createwiki-central' );
+	}
+	
+	public function getGlobalPrimaryDB(): IDatabase {
+		return $this->connectionProvider->getPrimaryDatabase( 'virtual-createwiki' );
+	}
+
+	public function getGlobalReplicaDB(): IReadableDatabase {
+		return $this->connectionProvider->getReplicaDatabase( 'virtual-createwiki' );
 	}
 
 	public function isCurrentWikiCentral(): bool {
 		return WikiMap::isCurrentWikiDbDomain( $this->getCentralWikiID() );
+	}
+
+	public function isRemoteWikiCentral( string $wiki ): bool {
+		return $wiki === $this->getCentralWikiID();
 	}
 }
