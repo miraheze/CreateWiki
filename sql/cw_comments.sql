ef2e18d1489628754190ce5daccabc75fa8a1c98
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-02-17 10:30:16 -0700

    Update

--- sql/cw_comments.sql
+++ sql/cw_comments.sql
@@ -1,6 +1,8 @@
 CREATE TABLE /*_*/cw_comments (
+  cw_comment_id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
   cw_id BIGINT UNSIGNED NOT NULL,
   cw_comment BLOB NOT NULL,
   cw_comment_timestamp BINARY(14) NOT NULL,
-  cw_comment_user INT(10) NOT NULL
+  cw_comment_actor INT(10) NOT NULL,
+  PRIMARY KEY(cw_comment_id)
 ) /*$wgDBTableOptions*/;
