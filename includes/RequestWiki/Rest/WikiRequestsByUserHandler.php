14920cf08e46cdb0316bd104918e5c759d85421a
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-02-17 12:23:20 -0700

    Move

--- includes/RequestWiki/Rest/WikiRequestsByUserHandler.php
+++ includes/RequestWiki/Rest/WikiRequestsByUserHandler.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Miraheze\CreateWiki\RequestWiki\Handler;
+namespace Miraheze\CreateWiki\RequestWiki\Rest;
 
 use MediaWiki\Rest\Response;
 use MediaWiki\Rest\SimpleHandler;
@@ -14,7 +14,7 @@ use Wikimedia\ParamValidator\ParamValidator;
  * Returns the IDs and suppression level of all wiki requests made by a user
  * GET /createwiki/v0/wiki_requests/user/{username}
  */
-class RestWikiRequestsByUser extends SimpleHandler {
+class WikiRequestsByUserHandler extends SimpleHandler {
 
 	public function __construct(
 		private readonly CreateWikiRestUtils $restUtils,
