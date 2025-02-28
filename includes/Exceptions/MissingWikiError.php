384d54d68db3353f2d176ddcbe1d5f4e20b8936e
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-02-28 14:05:23 -0700

    Only pass wiki to MissingWikiError (#680)
    Improves type hinting (ensuring wiki is a string) and reduces
    duplication (don't have to manually have to keep passing the same
    message key)

--- includes/Exceptions/MissingWikiError.php
+++ includes/Exceptions/MissingWikiError.php
@@ -4,7 +4,7 @@ namespace Miraheze\CreateWiki\Exceptions;
 
 class MissingWikiError extends ErrorBase {
 
-	public function __construct( string $msg, array $params ) {
-		parent::__construct( $msg, $params );
+	public function __construct( string $wiki ) {
+		parent::__construct( 'createwiki-error-missingwiki', [ $wiki ] );
 	}
 }
