9627c98c85888a731e2a499e18a37c7048e2743d
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-03-08 18:16:50 -0700

    CheckLastWikiActivity: remove isQuiet check

--- maintenance/CheckLastWikiActivity.php
+++ maintenance/CheckLastWikiActivity.php
@@ -15,9 +15,7 @@ class CheckLastWikiActivity extends Maintenance {
 	}
 
 	public function execute(): void {
-		if ( !$this->isQuiet() ) {
-			$this->output( (string)$this->getTimestamp() );
-		}
+		$this->output( (string)$this->getTimestamp() );
 	}
 
 	public function getTimestamp(): int {
