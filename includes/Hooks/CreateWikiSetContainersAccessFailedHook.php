83de27d339080f4c75c85f6cb32c73069a870434
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-03-19 12:26:44 -0600

    Fix

--- includes/Hooks/CreateWikiSetContainersAccessFailedHook.php
+++ includes/Hooks/CreateWikiSetContainersAccessFailedHook.php
@@ -7,12 +7,7 @@ interface CreateWikiSetContainersAccessFailedHook {
 	/**
 	 * @param string $dir
 	 * @param string $zone
-	 * @param array $errors
 	 * @return bool
 	 */
-	public function onCreateWikiSetContainersAccessFailed(
-		string $dir,
-		string $zone,
-		array $errors
-	): bool;
+	public function onCreateWikiSetContainersAccessFailed( string $dir, string $zone ): bool;
 }
