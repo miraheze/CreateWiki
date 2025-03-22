3e177f069c10d80692afbceb09f3170e9d424bc5
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-03-22 14:02:08 -0600

    Add unit test for PopulateCentralWiki

---
+++ tests/phpunit/Unit/Maintenance/PopulateCentralWikiTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Miraheze\CreateWiki\Tests\Unit\Maintenance;
+
+use MediaWikiUnitTestCase;
+use Miraheze\CreateWiki\Maintenance\PopulateCentralWiki;
+use Wikimedia\TestingAccessWrapper;
+
+/**
+ * @covers \Miraheze\CreateWiki\Maintenance\PopulateCentralWiki
+ * @group CreateWiki
+ */
+class PopulateCentralWikiTest extends MediaWikiUnitTestCase {
+
+	public function testGetUpdateKey() {
+		// Verifies that the update key does not change without deliberate meaning, as it could
+		// cause the script to be unnecessarily re-run on a new call to update.php.
+		$objectUnderTest = new PopulateCentralWiki();
+		$objectUnderTest = TestingAccessWrapper::newFromObject( $objectUnderTest );
+		$this->assertSame(
+			PopulateCentralWiki::class . ':wikidb',
+			$objectUnderTest->getUpdateKey(),
+			'::getUpdateKey did not return the expected key.'
+		);
+	}
+}
