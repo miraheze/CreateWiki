b34931770c70bdce4a170063cd390cdbff3e91ba
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-04-30 18:58:27 -0600

    Add TODO

--- includes/ICoreModule.php
+++ includes/ICoreModule.php
@@ -5,5 +5,7 @@ namespace Miraheze\CreateWiki;
 // This will eventually be moved to ManageWiki and
 // only loaded here when we are registering a provider
 // for ManageWiki core when this becomes possible.
+// TODO: add all the methods that should also be present
+// for ManageWiki core.
 interface ICoreModule extends IConfigModule {
 }
