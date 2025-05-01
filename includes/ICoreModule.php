58c25f0af240c304589621164c65d61286c0b981
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-04-30 18:52:28 -0600

    Add ICoreModule interface

---
+++ includes/ICoreModule.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Miraheze\CreateWiki;
+
+// This will eventually be moved to ManageWiki and
+// only loaded here when we are registering a provider
+// for ManageWiki core when this becomes possible.
+interface ICoreModule extends IConfigModule {
+}
