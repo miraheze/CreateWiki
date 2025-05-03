f9669f9a1296265e2257cb2a8fb94829d39c702a
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-05-03 16:18:35 -0600

    Update

--- includes/Helpers/ManageWikiCoreModule.php
+++ includes/Helpers/ManageWikiCoreModule.php
@@ -2,7 +2,21 @@
 
 namespace Miraheze\CreateWiki\Helpers;
 
+use Miraheze\CreateWiki\ConfigNames;
 use Miraheze\ManageWiki\ICoreModule;
 
 class ManageWikiCoreModule extends RemoteWiki implements ICoreModule {
+
+	public function isEnabled( string $feature ): bool {
+		// Enable all features
+		return true;
+	}
+
+	public function getCategoryOptions(): array {
+		return $this->options->get( ConfigNames::Categories );
+	}
+
+	public function getDatabaseClusters(): array {
+		return $this->options->get( ConfigNames::DatabaseClusters );
+	}
 }
