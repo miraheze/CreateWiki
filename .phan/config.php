26704014868a61a7c3fadc08d94181804a873188
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-05-03 14:24:52 -0600

    Add

--- .phan/config.php
+++ .phan/config.php
@@ -7,12 +7,14 @@ $cfg['minimum_target_php_version'] = '8.1';
 $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'], [
 		'../../extensions/Echo',
+		'../../extensions/ManageWiki',
 	]
 );
 
 $cfg['exclude_analysis_directory_list'] = array_merge(
 	$cfg['exclude_analysis_directory_list'], [
 		'../../extensions/Echo',
+		'../../extensions/ManageWiki',
 	]
 );
 
