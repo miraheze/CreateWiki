26babfc3037b9f4e3ea76b488383199ce622d3f4
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-06-01 03:24:37 -0600

    Add comment

--- includes/Hooks/CreateWikiSetContainersAccessFailedHook.php
+++ includes/Hooks/CreateWikiSetContainersAccessFailedHook.php
@@ -7,7 +7,7 @@ interface CreateWikiSetContainersAccessFailedHook {
 	/**
 	 * @param string $dir
 	 * @param string $zone
-	 * @return bool
+	 * @return bool Whether to retry the script again after running this hook.
 	 * @codeCoverageIgnore Cannot be annotated as covered.
 	 */
 	public function onCreateWikiSetContainersAccessFailed( string $dir, string $zone ): bool;
