miraheze/CreateWiki

commit fd12cecebdc6208e02d4da8a6279e5dc18ce2ab3

Major rewrite/cleanup of CreateWiki (#567)
### Tasks this will complete:
* [T9997](https://issue-tracker.miraheze.org/T9997) <sub>"Move
Miraheze-specific default i18n in CreateWiki to MirahezeMagic
overrides"</sub>
* [T9998](https://issue-tracker.miraheze.org/T9998) <sub>"Move
$wgCreateWikiDatabaseClustersInactive to be defined in ManageWiki"</sub>
* [T12075](https://issue-tracker.miraheze.org/T12075) <sub>"Have
CreateWiki notify Wiki Creators when a requester has requested more than
X wikis"</sub>
* [T12296](https://issue-tracker.miraheze.org/T12296) <sub>"Add the
ability to store a history of past revisions of a wiki request's
description"</sub>
* [T12471](https://issue-tracker.miraheze.org/T12471) <sub>"A tick box
to disable reopening a request"</sub>
* [T12610](https://issue-tracker.miraheze.org/T12610) <sub>"Comments on
wiki requests are getting lost despite success messages being
shown"</sub> — maybe
* [T12721](https://issue-tracker.miraheze.org/T12721) <sub>"Request
cannot be handled if a subdomain exists"</sub>

#### This will also make this completely possible utilizing new hooks:
* [T9153](https://issue-tracker.miraheze.org/T9153) <sub>"Allow users to
pre-select some basic settings for their wiki from the wiki request
form"</sub>

#### This also does the initial steps for:
* [T9106](https://issue-tracker.miraheze.org/T9106) <sub>"Add button
linking to new wiki on CW email notification"</sub>
* [T9154](https://issue-tracker.miraheze.org/T9154) <sub>"Warn wiki
requesters if their request is likely to be declined due to
issues"</sub>
* [T9874](https://issue-tracker.miraheze.org/T9874) <sub>"Email
bureaucrats whose wikis haven't been edited at all in 1 week from
creation"</sub>
* [T10528](https://issue-tracker.miraheze.org/T10528) <sub>"Have
CreateWiki provide a notice about problematic users requesting
wikis"</sub>
* [T11290](https://issue-tracker.miraheze.org/T11290) <sub>"Detect if a

includes/Notifications/EchoRequestMoreDetailsPresentationModel.php
@@ -5,29 +5,45 @@ namespace Miraheze\CreateWiki\Notifications;
 use MediaWiki\Extension\Notifications\DiscussionParser;
 use MediaWiki\Extension\Notifications\Formatters\EchoEventPresentationModel;
 use MediaWiki\Language\RawMessage;
+use MediaWiki\Message\Message;
 
 class EchoRequestMoreDetailsPresentationModel extends EchoEventPresentationModel {
 
-	public function getIconType() {
+	/**
+	 * @inheritDoc
+	 */
+	public function getIconType(): string {
 		return 'global';
 	}
 
-	public function getHeaderMessage() {
+	/**
+	 * @inheritDoc
+	 */
+	public function getHeaderMessage(): Message {
 		return $this->msg( 'notification-header-request-moredetails' );
 	}
 
-	public function getBodyMessage() {
-		$reason = $this->event->getExtraParam( 'reason' );
-		$text = DiscussionParser::getTextSnippet( $reason, $this->language );
+	/**
+	 * @inheritDoc
+	 */
+	public function getBodyMessage(): RawMessage {
+		$comment = $this->event->getExtraParam( 'comment' );
+		$text = DiscussionParser::getTextSnippet( $comment, $this->language );
 
-		return new RawMessage( "$1", [ $text ] );
+		return new RawMessage( '$1', [ $text ] );
 	}
 
-	public function getPrimaryLink() {
+	/**
+	 * @inheritDoc
+	 */
+	public function getPrimaryLink(): bool {
 		return false;
 	}
 
-	public function getSecondaryLinks() {
+	/**
+	 * @inheritDoc
+	 */
+	public function getSecondaryLinks(): array {
 		$visitLink = [
 			'url' => $this->event->getExtraParam( 'request-url', 0 ),
 			'label' => $this->msg( 'notification-createwiki-visit-request' )->text(),

commit 32c8b01965131262875eed0523f432df66358798

Use 'reason' param if 'comment' is unset in RequestMoreDetails notification (#784)
This fixes a regression introduced in
fd12cecebdc6208e02d4da8a6279e5dc18ce2ab3.
Follow-up to 0b6468a

```
MessageCache::parseWithPostprocessing(): Argument #1 ($text) must be of type string, null given, called in /srv/mediawiki/1.44/extensions/Echo/includes/DiscussionParser.php on line 1279
from /srv/mediawiki/1.44/includes/language/MessageCache.php(1434)
#0 /srv/mediawiki/1.44/extensions/Echo/includes/DiscussionParser.php(1279): MessageCache->parseWithPostprocessing(null, MediaWiki\Page\PageReferenceValue, bool, bool, LanguageEn)
#1 /srv/mediawiki/1.44/extensions/CreateWiki/includes/Notifications/EchoRequestMoreDetailsPresentationModel.php(25): MediaWiki\Extension\Notifications\DiscussionParser::getTextSnippet(null, LanguageEn)
#2 /srv/mediawiki/1.44/extensions/Echo/includes/Formatters/EchoEventPresentationModel.php(570): Miraheze\CreateWiki\Notifications\EchoRequestMoreDetailsPresentationModel->getBodyMessage()
#3 /srv/mediawiki/1.44/extensions/Echo/includes/Formatters/EchoModelFormatter.php(14): MediaWiki\Extension\Notifications\Formatters\EchoEventPresentationModel->jsonSerialize()
#4 /srv/mediawiki/1.44/extensions/Echo/includes/Formatters/EchoEventFormatter.php(77): MediaWiki\Extension\Notifications\Formatters\EchoModelFormatter->formatModel(Miraheze\CreateWiki\Notifications\EchoRequestMoreDetailsPresentationModel)
#5 /srv/mediawiki/1.44/extensions/Echo/includes/DataOutputFormatter.php(201): MediaWiki\Extension\Notifications\Formatters\EchoEventFormatter->format(MediaWiki\Extension\Notifications\Model\Event)
#6 /srv/mediawiki/1.44/extensions/Echo/includes/DataOutputFormatter.php(160): MediaWiki\Extension\Notifications\DataOutputFormatter::formatNotification(MediaWiki\Extension\Notifications\Model\Event, MediaWiki\User\User, string, LanguageEn)

includes/Notifications/EchoRequestMoreDetailsPresentationModel.php
@@ -21,7 +21,7 @@ class EchoRequestMoreDetailsPresentationModel extends EchoEventPresentationModel
 
 	/** @inheritDoc */
 	public function getBodyMessage(): RawMessage {
-		$comment = $this->event->getExtraParam( 'comment' );
+		$comment = $this->event->getExtraParam( 'comment' ) ?? $this->event->getExtraParam( 'reason' );
 		$text = DiscussionParser::getTextSnippet( $comment, $this->language );
 
 		return new RawMessage( '$1', [ $text ] );