720a74a6d1ac138bc9154e954e9c92186342dad8
Reception123 <utilizator.receptie123@gmail.com>
2026-06-24 14:12:53 +0200

    Update patch-cw_wikis-add-wiki_inactive_exempt_expiry.sql

--- sql/patches/patch-cw_wikis-add-wiki_inactive_exempt_expiry.sql
+++ sql/patches/patch-cw_wikis-add-wiki_inactive_exempt_expiry.sql
@@ -1,2 +1,2 @@
 ALTER TABLE /*$wgDBprefix*/cw_wikis
-  ADD COLUMN wiki_inactive_exempt_expiry TEXT NULL AFTER wiki_inactive_exempt_reason;
+  ADD COLUMN wiki_inactive_exempt_expiry VARBINARY(14) NULL AFTER wiki_inactive_exempt_reason;
