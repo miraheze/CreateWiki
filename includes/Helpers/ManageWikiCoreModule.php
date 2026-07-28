2fc3c4bb204c836a5e5cc1bd44788cf234ac5351
Reception123 <utilizator.receptie123@gmail.com>
2026-07-28 19:55:35 +0200

    Revert "add option to record closure reasons (T15629) (#828)"
    This reverts commit 76d61ba463e8553ea107326d81f292b975cc81fb.

--- includes/Helpers/ManageWikiCoreModule.php
+++ includes/Helpers/ManageWikiCoreModule.php
@@ -33,8 +33,4 @@ class ManageWikiCoreModule extends RemoteWiki implements ICoreModule {
 	public function getInactiveExemptReasonOptions(): array {
 		return $this->options->get( ConfigNames::InactiveExemptReasonOptions );
 	}
-
-	public function getClosedReasonOptions(): array {
-		return $this->options->get( ConfigNames::ClosedReasonOptions );
-	}
 }
