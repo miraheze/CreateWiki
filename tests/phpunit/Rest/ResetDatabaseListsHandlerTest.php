2394e4f836b47669e794c9ad33c5ffc9d38e8cd5
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-09-06 15:48:27 -0600

    add test

---
+++ tests/phpunit/Rest/ResetDatabaseListsHandlerTest.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Miraheze\CreateWiki\Tests\Rest;
+
+use MediaWiki\Rest\LocalizedHttpException;
+use MediaWiki\Rest\RequestData;
+use MediaWiki\Tests\Rest\Handler\HandlerTestTrait;
+use MediaWikiIntegrationTestCase;
+use Miraheze\CreateWiki\Rest\CacheRestUtils;
+use Miraheze\CreateWiki\Rest\ResetDatabaseListsHandler;
+use Miraheze\CreateWiki\Services\CreateWikiDataStore;
+use function json_encode;
+
+/**
+ * @group CreateWiki
+ * @coversDefaultClass \Miraheze\CreateWiki\Rest\ResetDatabaseListsHandler
+ */
+class ResetDatabaseListsHandlerTest extends MediaWikiIntegrationTestCase {
+	use HandlerTestTrait;
+
+	private function newRequest( array $body ): RequestData {
+		return new RequestData( [
+			'method' => 'POST',
+			'bodyContents' => json_encode( $body ),
+			'headers' => [ 'Content-Type' => 'application/json' ],
+		] );
+	}
+
+	/**
+	 * @covers ::run
+	 * @covers ::getBodyParamSettings
+	 */
+	public function testDisabledReturns404(): void {
+		$restUtils = $this->createMock( CacheRestUtils::class );
+		$restUtils->method( 'isRestEnabled' )->willReturn( false );
+
+		$dataStore = $this->createMock( CreateWikiDataStore::class );
+		$handler = new ResetDatabaseListsHandler( $restUtils, $dataStore );
+
+		$this->expectException( LocalizedHttpException::class );
+		$this->expectExceptionCode( 404 );
+		$this->executeHandler(
+			$handler,
+			$this->newRequest( [ 'key' => 'x', 'name' => 'databases' ] )
+		);
+	}
+
+	/**
+	 * @covers ::run
+	 */
+	public function testThrottledReturns429(): void {
+		$restUtils = $this->createMock( CacheRestUtils::class );
+		$restUtils->method( 'isRestEnabled' )->willReturn( true );
+		$restUtils->method( 'isThrottled' )->willReturn( true );
+
+		$dataStore = $this->createMock( CreateWikiDataStore::class );
+		$handler = new ResetDatabaseListsHandler( $restUtils, $dataStore );
+
+		$this->expectException( LocalizedHttpException::class );
+		$this->expectExceptionCode( 429 );
+		$this->executeHandler(
+			$handler,
+			$this->newRequest( [ 'key' => 'x', 'name' => 'databases' ] )
+		);
+	}
+
+	/**
+	 * @covers ::run
+	 */
+	public function testInvalidKeyReturns403(): void {
+		$restUtils = $this->createMock( CacheRestUtils::class );
+		$restUtils->method( 'isRestEnabled' )->willReturn( true );
+		$restUtils->method( 'isThrottled' )->willReturn( false );
+		$restUtils->method( 'isValidKey' )->willReturn( false );
+		$restUtils->expects( $this->once() )->method( 'recordFailure' );
+
+		$dataStore = $this->createMock( CreateWikiDataStore::class );
+		$handler = new ResetDatabaseListsHandler( $restUtils, $dataStore );
+
+		$this->expectException( LocalizedHttpException::class );
+		$this->expectExceptionCode( 403 );
+		$this->executeHandler(
+			$handler,
+			$this->newRequest( [ 'key' => 'wrong', 'name' => 'databases' ] )
+		);
+	}
+
+	/**
+	 * @covers ::run
+	 */
+	public function testValidPayloadAppliesDatabaseList(): void {
+		$restUtils = $this->createMock( CacheRestUtils::class );
+		$restUtils->method( 'isRestEnabled' )->willReturn( true );
+		$restUtils->method( 'isThrottled' )->willReturn( false );
+		$restUtils->method( 'isValidKey' )->willReturn( true );
+
+		$list = [ 'mtime' => 1, 'databases' => [] ];
+
+		$dataStore = $this->createMock( CreateWikiDataStore::class );
+		$dataStore->expects( $this->once() )
+			->method( 'applyDatabaseList' )
+			->with( 'databases', $list );
+		$dataStore->expects( $this->never() )->method( 'resetDatabaseLists' );
+
+		$handler = new ResetDatabaseListsHandler( $restUtils, $dataStore );
+
+		$response = $this->executeHandler(
+			$handler,
+			$this->newRequest( [
+				'key' => 'correct',
+				'name' => 'databases',
+				'data' => json_encode( $list ),
+			] )
+		);
+
+		$this->assertSame( 204, $response->getStatusCode() );
+	}
+
+	/**
+	 * @covers ::run
+	 */
+	public function testNoPayloadFallsBackToDatabaseQuery(): void {
+		$restUtils = $this->createMock( CacheRestUtils::class );
+		$restUtils->method( 'isRestEnabled' )->willReturn( true );
+		$restUtils->method( 'isThrottled' )->willReturn( false );
+		$restUtils->method( 'isValidKey' )->willReturn( true );
+
+		$dataStore = $this->createMock( CreateWikiDataStore::class );
+		$dataStore->expects( $this->never() )->method( 'applyDatabaseList' );
+		$dataStore->expects( $this->once() )
+			->method( 'resetDatabaseLists' )
+			->with( false, false )
+			->willReturn( true );
+
+		$handler = new ResetDatabaseListsHandler( $restUtils, $dataStore );
+
+		$response = $this->executeHandler(
+			$handler,
+			$this->newRequest( [ 'key' => 'correct', 'name' => 'databases' ] )
+		);
+
+		$this->assertSame( 204, $response->getStatusCode() );
+	}
+}
