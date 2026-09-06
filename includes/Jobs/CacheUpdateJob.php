601951c6ccec44eb3d4c210742c34bab86d42787
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-09-06 16:29:22 -0600

    order

--- includes/Jobs/CacheUpdateJob.php
+++ includes/Jobs/CacheUpdateJob.php
@@ -9,16 +9,16 @@ class CacheUpdateJob extends Job {
 
 	public const string JOB_NAME = 'CreateWikiCacheUpdateJob';
 
-	private readonly ?string $data;
 	private readonly string $name;
+	private readonly ?string $data;
 
 	public function __construct(
 		array $params,
 		private readonly CacheUpdate $cacheUpdate,
 	) {
 		parent::__construct( self::JOB_NAME, $params );
-		$this->data = $params['data'] ?? null;
 		$this->name = $params['name'];
+		$this->data = $params['data'] ?? null;
 	}
 
 	public function run(): bool {
