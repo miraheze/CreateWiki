de1ecffb4b95094f88b3f17ec3f6c22653c5c67b
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-09-07 16:30:59 -0600

    Add

---
+++ includes/RequestWiki/Rest/RequestWikiValidateFieldHandler.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Miraheze\CreateWiki\RequestWiki\Rest;
+
+use MediaWiki\Message\Message;
+use MediaWiki\Rest\Response;
+use MediaWiki\Rest\SimpleHandler;
+use MediaWiki\Rest\TokenAwareHandlerTrait;
+use MediaWiki\Rest\Validator\Validator;
+use Miraheze\CreateWiki\Services\CreateWikiRestUtils;
+use Miraheze\CreateWiki\Services\CreateWikiValidator;
+use Wikimedia\Message\MessageValue;
+use Wikimedia\ParamValidator\ParamValidator;
+
+/**
+ * Validates a single RequestWiki field ahead of full submission
+ * POST /createwiki/v0/request_wiki/validate
+ */
+class RequestWikiValidateFieldHandler extends SimpleHandler {
+
+	use TokenAwareHandlerTrait;
+
+	public function __construct(
+		private readonly CreateWikiRestUtils $restUtils,
+		private readonly CreateWikiValidator $validator,
+	) {
+	}
+
+	/** @inheritDoc */
+	public function validate( Validator $restValidator ) {
+		parent::validate( $restValidator );
+		$this->validateToken();
+	}
+
+	public function run(): Response {
+		$this->restUtils->checkEnv();
+
+		if ( !$this->getAuthority()->isNamed() ) {
+			return $this->getResponseFactory()->createLocalizedHttpError(
+				403, new MessageValue( 'createwiki-rest-mustlogin' )
+			);
+		}
+
+		if ( $this->getAuthority()->getBlock() ) {
+			return $this->getResponseFactory()->createLocalizedHttpError(
+				403, new MessageValue( 'createwiki-rest-notallowed' )
+			);
+		}
+
+		$body = $this->getValidatedBody();
+		$value = $body['value'];
+
+		$result = match ( $body['field'] ) {
+			'subdomain' => $this->validator->validateSubdomain( $value, [] ),
+			'reason' => $this->validator->validateReason( $value, [] ),
+			'category', 'purpose' => $this->validator->validateRequired( $value ),
+			default => true,
+		};
+
+		if ( $result === true ) {
+			return $this->getResponseFactory()->createJson( [ 'valid' => true ] );
+		}
+
+		return $this->getResponseFactory()->createJson( [
+			'valid' => false,
+			'message' => $result instanceof Message ? $result->parse() : (string)$result,
+		] );
+	}
+
+	public function needsWriteAccess(): bool {
+		return false;
+	}
+
+	public function getBodyParamSettings(): array {
+		return [
+			'field' => [
+				self::PARAM_SOURCE => 'body',
+				ParamValidator::PARAM_TYPE => [ 'subdomain', 'reason', 'category', 'purpose' ],
+				ParamValidator::PARAM_REQUIRED => true,
+			],
+			'value' => [
+				self::PARAM_SOURCE => 'body',
+				ParamValidator::PARAM_TYPE => 'string',
+				ParamValidator::PARAM_REQUIRED => true,
+			],
+		] + $this->getTokenParamDefinition();
+	}
+}
