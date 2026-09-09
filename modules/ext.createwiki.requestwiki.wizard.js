ea3a816b266708151b100de30b9212f9dbf067bd
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-09-09 16:01:05 -0600

    Update

--- modules/ext.createwiki.requestwiki.wizard.js
+++ modules/ext.createwiki.requestwiki.wizard.js
@@ -21,9 +21,9 @@
 		const rest = new mw.Rest();
 		const api = new mw.Api();
 
-		const nextWidget = OO.ui.infuse( $next );
-		const nextLabel = mw.msg( 'requestwiki-wizard-next' );
-		const startLabel = mw.msg( 'requestwiki-wizard-start' );
+		const $nextLabel = $next.find( '.oo-ui-labelElement-label' );
+		const nextLabelText = mw.msg( 'requestwiki-wizard-next' );
+		const startLabelText = mw.msg( 'requestwiki-wizard-start' );
 
 		let current = 0;
 
@@ -92,7 +92,7 @@
 			$back.toggle( current > 0 );
 			$next.toggle( current < total - 1 );
 			$submit.toggle( current === total - 1 );
-			nextWidget.setLabel( current === 0 ? startLabel : nextLabel );
+			$nextLabel.text( current === 0 ? startLabelText : nextLabelText );
 
 			if ( scroll ) {
 				$steps.eq( current ).get( 0 ).scrollIntoView( { behavior: 'smooth', block: 'start' } );
