ff0f016ad2c7dc7b2ca13898dbdfd76a30273339
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-09-11 11:29:37 -0600

    Update

--- modules/ext.createwiki.requestwiki.wizard.js
+++ modules/ext.createwiki.requestwiki.wizard.js
@@ -228,7 +228,7 @@
 
 			$steps.each( function ( stepIndex ) {
 				const stepKey = $( this ).data( 'step' );
-				if ( stepKey === 'intro' || stepKey === 'agreement' ) {
+				if ( stepKey === 'intro' || stepKey === 'review' ) {
 					return;
 				}
 
