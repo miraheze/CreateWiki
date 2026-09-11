4bc0950bd23be596d7c866a4910ce538d2414837
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-09-11 11:48:06 -0600

    Update

--- modules/ext.createwiki.requestwiki.wizard.js
+++ modules/ext.createwiki.requestwiki.wizard.js
@@ -474,7 +474,7 @@
 			}
 
 			if ( stepKey === 'intro' ) {
-				addCheck( 'ratelimited', '', $stepErrorAnchor );
+				addCheck( 'throttled', '', $stepErrorAnchor );
 			}
 
 			if ( stepKey === 'basics' ) {
