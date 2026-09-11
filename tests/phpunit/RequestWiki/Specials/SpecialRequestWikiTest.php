a6261069dc50798aebf559aa921f86dbcdd9f29b
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-09-11 13:08:06 -0600

    Fix

--- tests/phpunit/RequestWiki/Specials/SpecialRequestWikiTest.php
+++ tests/phpunit/RequestWiki/Specials/SpecialRequestWikiTest.php
@@ -116,7 +116,11 @@ class SpecialRequestWikiTest extends SpecialPageTestBase {
 			'wpreason' => 'Test onSuccess() via execute()',
 		] ) );
 
-		$context->setUser( $this->getTestUserAuthorityWithConfirmedEmail()->getUser() );
+		$user = $this->getServiceContainer()->getUserFactory()->newFromUserIdentity(
+			$this->getTestUserAuthorityWithConfirmedEmail()->getUser()
+		);
+
+		$context->setUser( $user );
 		$this->executeSpecialPage( '', null, null, null, false, $context );
 
 		$expectedUrl = SpecialPage::getTitleFor( 'RequestWikiQueue', '1' )->getFullURL();
